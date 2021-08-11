7cbbef960259bb0250b0b6ab464e35b253e78372
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-11 22:23:57 +0900

    chore: 폴더 구조 정의

--- client/src/components/App.tsx
+++
@@ -1,5 +0,0 @@
-import React, { ReactElement } from 'react';
-
-const App = (): ReactElement => <div>App</div>;
-
-export default App;
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Home = (): React.ReactElement => {
+  return <div>Home</div>;
+};
+
+export default Home;
