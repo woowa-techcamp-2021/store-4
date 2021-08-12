d8abbfc745b122759490d82d04398e2f579a4471
momongss <rkrp15@gmail.com>
2021-08-12 17:37:18 +0900

    add: mock 데이터

---
+++ client/src/components/ProductList/ProductMock.ts
@@ -0,0 +1,47 @@
+import MockImg from '../../assets/images/towel.png';
+import { ProductItemType } from '../../types/product';
+
+export const mockProductList: Array<ProductItemType> = [
+  {
+    id: 0,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 2000,
+    uploadDate: '2018-02-08',
+    imgSrc: MockImg,
+  },
+  {
+    id: 1,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 1000,
+    uploadDate: '2018-02-09',
+    imgSrc: MockImg,
+  },
+  {
+    id: 2,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 4000,
+    uploadDate: '2018-01-09',
+    imgSrc: MockImg,
+  },
+  {
+    id: 3,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 1000,
+    uploadDate: '2021-06-10',
+    imgSrc: MockImg,
+  },
+  {
+    id: 4,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 10000,
+    uploadDate: '2021-04-17',
+    imgSrc: MockImg,
+  },
+  {
+    id: 5,
+    name: '컴퓨터싸인펜. 오늘이 전설이 될 것이다',
+    price: 500,
+    uploadDate: '2021-01-19',
+    imgSrc: MockImg,
+  },
+];
