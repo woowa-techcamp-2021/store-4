bbccfd6b97b4e51e64e82090c464ac9cc53d27cb
sanginchun <sanginchun91@gmail.com>
2021-08-12 20:56:22 +0900

    add: Jwt 토큰 생성 메서드 추가

--- server/src/services/jwt.service.ts
+++ server/src/services/jwt.service.ts
@@ -5,7 +5,15 @@ interface CustomJwtPayload extends JwtPayload {
   name: string;
 }
 
+const TOKEN_EXPIRE = '2h';
+
 class JwtService {
+  generate(payload: CustomJwtPayload, key: string) {
+    return jwt.sign(payload, key, {
+      expiresIn: TOKEN_EXPIRE,
+    });
+  }
+
   decode(token: string) {
     return jwt.decode(token) as CustomJwtPayload;
   }
