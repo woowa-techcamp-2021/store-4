725ed6f168310b7cc5fe90bc861f181edb58f748
sanginchun <sanginchun91@gmail.com>
2021-08-13 13:56:33 +0900

    chore: user === undefined로 수정

--- server/src/services/user.service.ts
+++ server/src/services/user.service.ts
@@ -1,13 +1,12 @@
 import { getCustomRepository } from 'typeorm';
-import User from '../models/user';
 import UserRepository from '../repositories/user-repository';
 
 class UserService {
   async checkUserByEmail(email: string): Promise<boolean> {
     const user = await getCustomRepository(UserRepository).findByEmail(email);
 
-    if (user instanceof User) return true;
-    else return false;
+    if (user === undefined) return false;
+    else return true;
   }
 
   async registerUser({ username, email }: { username: string; email: string }) {
