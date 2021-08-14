5119ca9f681c6afecdd5062d2344e4d4d9a24dda
sanginchun <sanginchun91@gmail.com>
2021-08-14 19:57:22 +0900

    add: pagination 기능 추가 및 에러 처리 - 논의가 필요할 것 같은 부분   - ProductResponse 타입 정의   - ERROR_TYPE enum 정의 (에러 처리 방식)

--- server/src/controllers/product-controller.ts
+++ server/src/controllers/product-controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import Product from '../models/product';
 import productService from '../services/product-service';
 
 export type ProductQuery = {
@@ -7,15 +8,34 @@ export type ProductQuery = {
   pageNum?: string;
 };
 
+export type ProductResponse = {
+  products: Product[];
+  totalPages: number;
+};
+
+export enum ERROR_TYPE {
+  INVALID_CATEGORY,
+  INVALID_PAGE,
+}
+
 class ProductController {
   async getAll(req: Request, res: Response) {
-    const { category, sort, pageNum } = req.query as ProductQuery;
+    try {
+      const { category, sort, pageNum } = req.query as ProductQuery;
 
-    const products = await productService.findAll({ category, sort, pageNum });
+      const productResponse = await productService.findAll({ category, sort, pageNum });
 
-    res.status(200).json({
-      products,
-    });
+      res.status(200).json(productResponse);
+    } catch (err) {
+      switch (err) {
+        case ERROR_TYPE.INVALID_CATEGORY:
+          return res.status(404).json({ message: '카테고리가 존재하지 않습니다' });
+        case ERROR_TYPE.INVALID_PAGE:
+          return res.status(404).json({ message: '요청한 페이지가 총 페이지 수를 초과했습니다' });
+        default:
+          throw err;
+      }
+    }
   }
 }
 
--- server/src/services/product-service.ts
+++ server/src/services/product-service.ts
@@ -1,12 +1,26 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../models/product';
 import ProductRepository from '../repositories/product-repository';
-import { ProductQuery } from '../controllers/product-controller';
+import { ProductQuery, ProductResponse, ERROR_TYPE } from '../controllers/product-controller';
+
+const ITEMS_PER_PAGE = 20;
 
 class ProductService {
-  async findAll({ category, sort, pageNum }: ProductQuery): Promise<Product[]> {
+  async findAll({ category, sort, pageNum }: ProductQuery): Promise<ProductResponse> {
     const productDataFilteredByCategory = await this.findByCategory(category);
-    return productDataFilteredByCategory;
+    if (productDataFilteredByCategory.length === 0) throw ERROR_TYPE.INVALID_CATEGORY;
+
+    const page = pageNum === undefined ? 1 : +pageNum;
+    const totalPages = this.getTotalPages(productDataFilteredByCategory);
+
+    if (page > totalPages) throw ERROR_TYPE.INVALID_PAGE;
+
+    const productData = productDataFilteredByCategory.slice(
+      ITEMS_PER_PAGE * (page - 1),
+      ITEMS_PER_PAGE * page
+    );
+
+    return { products: productData, totalPages };
   }
 
   private findByCategory(categoryId: string | undefined) {
@@ -15,6 +29,10 @@ class ProductService {
     if (categoryId === undefined) return productRepository.find();
 
     return productRepository.findByCategory(+categoryId);
+  }
+
+  private getTotalPages(products: Product[]): number {
+    return Math.ceil(products.length / ITEMS_PER_PAGE);
   }
 }
 
