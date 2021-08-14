c5cd71114d5dd6c7697d7fec0c212edae98f727c
sanginchun <sanginchun91@gmail.com>
2021-08-15 01:06:23 +0900

    add: 상품 판매량 조회 로직

---
+++ server/src/services/order-detail-service.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from 'typeorm';
+import OrderDetailRepository from '../repositories/order-detail-repository';
+
+class OrderDetailService {
+  async getTotalOrderCountOfProduct(productId: string): Promise<number> {
+    const orderDetails = await getCustomRepository(OrderDetailRepository).findByProduct(productId);
+    if (orderDetails.length === 0) return 0;
+
+    return orderDetails.map(({ quantity }) => quantity).reduce((acc, val) => acc + val, 0);
+  }
+}
+
+export default new OrderDetailService();
