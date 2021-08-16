e9b7d36c77fb98be5ac0ab1e0d8daa7adfb4e84c
sanginchun <sanginchun91@gmail.com>
2021-08-16 17:22:13 +0900

    chore: 판매량 계산 함수 리팩토링

--- server/src/services/order-detail-service.ts
+++ server/src/services/order-detail-service.ts
@@ -4,9 +4,8 @@ import OrderDetailRepository from '../repositories/order-detail-repository';
 class OrderDetailService {
   async countTotalSalesOfProduct(productId: string): Promise<number> {
     const orderDetails = await getCustomRepository(OrderDetailRepository).findByProduct(productId);
-    if (orderDetails.length === 0) return 0;
 
-    return orderDetails.map(({ quantity }) => quantity).reduce((acc, val) => acc + val, 0);
+    return orderDetails.reduce((acc, val) => acc + val.quantity, 0);
   }
 }
 
