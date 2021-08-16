8a390432c1d32a81ef97fb5d2eae8988fa85db30
sanginchun <sanginchun91@gmail.com>
2021-08-17 01:03:32 +0900

    add: 카테고리 레이어 최소 기능

--- client/src/components/Header/HeaderMain/CategoryMenu/CategoryMenu.tsx
+++ client/src/components/Header/HeaderMain/CategoryMenu/CategoryMenu.tsx
@@ -1,7 +1,55 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import menuIcon from './menuIcon.png';
+import CategoryLayer from './CategoryLayer';
+
+const Wrapper = styled.div`
+  position: relative;
+
+  .button {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+
+    .icon {
+      width: 20px;
+      height: auto;
+
+      margin-right: 8px;
+    }
+
+    .text {
+      font-size: 14px;
+      color: #333;
+    }
+  }
+`;
 
 const CategoryMenu = (): JSX.Element => {
-  return <div>CategoryMenu</div>;
+  const [showLayer, setShowLayer] = useState(false);
+  const handleLayerButtonClick = useCallback(
+    () => setShowLayer(!showLayer),
+    [showLayer, setShowLayer]
+  );
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOnShowLayer);
+
+    function handleClickOnShowLayer() {
+      if (showLayer) setShowLayer(false);
+    }
+    return () => document.removeEventListener('click', handleClickOnShowLayer);
+  }, [showLayer, setShowLayer]);
+
+  return (
+    <Wrapper>
+      <div className="button" onClick={handleLayerButtonClick}>
+        <img className="icon" src={menuIcon} />
+        <span className="text">전체 카테고리</span>
+      </div>
+      {showLayer ? <CategoryLayer /> : null}
+    </Wrapper>
+  );
 };
 
 export default CategoryMenu;
