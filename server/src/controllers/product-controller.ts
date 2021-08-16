397512574c193aea855d033266a7b6c5778140a7
sanginchun <sanginchun91@gmail.com>
2021-08-16 18:32:41 +0900

    add: 상품 컨트롤러, 서비스 역할 수정 - 컨트롤러에서 쿼리 검증 후 서비스로 넘겨주도록 변경 - 위 변경에 따른 상수화, 타입 선언 등 추가 및 삭제

--- server/src/controllers/product-controller.ts
+++ server/src/controllers/product-controller.ts
@@ -2,10 +2,33 @@ import { Request, Response } from 'express';
 import Product from '../models/product';
 import productService from '../services/product-service';
 
-export type ProductQuery = {
+type ProductRequestQuery = {
   category?: string;
   sort?: string;
   pageNum?: string;
+  limit?: string;
+};
+
+export type FIND_OPTION = {
+  categoryId: number;
+  sortOption: number;
+  pageNum: number;
+  limit: number;
+};
+
+export const SORT_OPTIONS: { [key: string]: number } = {
+  recommend: 0,
+  popularity: 1,
+  recent: 2,
+  priceLow: 3,
+  priceHigh: 4,
+};
+
+const DEFAULT_OPTIONS = {
+  categoryId: -1,
+  sortOption: SORT_OPTIONS.recommend,
+  pageNum: 1,
+  limit: 20,
 };
 
 export type ProductResponse = {
@@ -18,14 +41,15 @@ export enum ERROR_TYPE {
   INVALID_PAGE,
   INVALID_SORT,
   PAGE_OVERFLOW,
+  INVALID_LIMIT,
 }
 
 class ProductController {
   async getAll(req: Request, res: Response) {
     try {
-      const { category, sort, pageNum } = req.query as ProductQuery;
+      const findOption = this.buildFindOption(req.query as ProductRequestQuery);
 
-      const productResponse = await productService.findAll({ category, sort, pageNum });
+      const productResponse = await productService.findAll(findOption);
 
       res.status(200).json(productResponse);
     } catch (err) {
@@ -38,10 +62,30 @@ class ProductController {
           return res.status(400).json({ message: '요청한 정렬 방식이 잘못되었습니다' });
         case ERROR_TYPE.INVALID_PAGE:
           return res.status(400).json({ message: '요청한 페이지 형식이 잘못되었습니다' });
+        case ERROR_TYPE.INVALID_LIMIT:
+          return res.status(400).json({ message: '요청한 리밋 형식이 잘못되었습니다' });
         default:
           throw err;
       }
     }
+  }
+
+  buildFindOption(query: ProductRequestQuery): FIND_OPTION {
+    const { category, sort, pageNum, limit } = query;
+
+    const categoryId = category === undefined ? DEFAULT_OPTIONS.categoryId : +category;
+    if (isNaN(categoryId)) throw ERROR_TYPE.INVALID_CATEGORY;
+
+    const sortOption = sort === undefined ? DEFAULT_OPTIONS.sortOption : SORT_OPTIONS[sort];
+    if (sortOption === undefined) throw ERROR_TYPE.INVALID_SORT;
+
+    const _pageNum = pageNum === undefined ? DEFAULT_OPTIONS.pageNum : +pageNum;
+    if (isNaN(_pageNum)) throw ERROR_TYPE.INVALID_PAGE;
+
+    const _limit = limit === undefined ? DEFAULT_OPTIONS.limit : +limit;
+    if (isNaN(_limit)) throw ERROR_TYPE.INVALID_LIMIT;
+
+    return { categoryId, sortOption, pageNum: _pageNum, limit: _limit };
   }
 }
 
--- server/src/services/product-service.ts
+++ server/src/services/product-service.ts
@@ -1,117 +1,116 @@
 import { getCustomRepository } from 'typeorm';
 import Product from '../models/product';
 import ProductRepository from '../repositories/product-repository';
-import { ProductQuery, ProductResponse, ERROR_TYPE } from '../controllers/product-controller';
+import {
+  ProductResponse,
+  FIND_OPTION,
+  SORT_OPTIONS,
+  ERROR_TYPE,
+} from '../controllers/product-controller';
 import ReviewService from './review-service';
 import OrderDetailService from './order-detail-service';
 
-const SORT_OPTIONS = ['recommend', 'popularity', 'recent', 'priceLow', 'priceHigh'];
-const ITEMS_PER_PAGE = 20;
-
 class ProductService {
-  async findAll({ category, sort, pageNum }: ProductQuery): Promise<ProductResponse> {
-    const productsByCategory = await this.filterProductsByCategory(category);
-
-    const productsSorted = await this.sortProducts(productsByCategory, sort);
-
-    const { products, totalPages } = this.paginateProducts(productsSorted, pageNum);
-
-    return { products, totalPages };
+  async findAll({ categoryId, sortOption, pageNum, limit }: FIND_OPTION): Promise<void> {
+    // const productsByCategory = await this.filterProductsByCategory(category);
+    // const productsSorted = await this.sortProducts(productsByCategory, sort);
+    // const { products, totalPages } = this.paginateProducts(productsSorted, pageNum);
+    // return { products, totalPages };
   }
 
-  /**
-   *
-   * @param categoryId URL query : 주어지지 않은 경우 전체 상품 목록 반환
-   * @returns 주어진 카테고리 ID에 해당하는 상품 목록
-   *
-   * @throws 주어진 카테고리 ID에 해당하는 상품이 없을 경우, 카테고리 ID가 유효하지 않은 것으로 간주하고 에러 발생
-   */
-  private async filterProductsByCategory(categoryId: string | undefined) {
-    const productRepository = getCustomRepository(ProductRepository);
+  // /**
+  //  *
+  //  * @param categoryId URL query : 주어지지 않은 경우 전체 상품 목록 반환
+  //  * @returns 주어진 카테고리 ID에 해당하는 상품 목록
+  //  *
+  //  * @throws 주어진 카테고리 ID에 해당하는 상품이 없을 경우, 카테고리 ID가 유효하지 않은 것으로 간주하고 에러 발생
+  //  */
+  // private async filterProductsByCategory(categoryId: string | undefined) {
+  //   const productRepository = getCustomRepository(ProductRepository);
 
-    if (categoryId === undefined) return productRepository.find();
+  //   if (categoryId === undefined) return productRepository.find();
 
-    const productsByCategory = await productRepository.findByCategory(+categoryId);
-    if (productsByCategory.length === 0) throw ERROR_TYPE.INVALID_CATEGORY;
+  //   const productsByCategory = await productRepository.findByCategory(+categoryId);
+  //   if (productsByCategory.length === 0) throw ERROR_TYPE.INVALID_CATEGORY;
 
-    return productsByCategory;
-  }
+  //   return productsByCategory;
+  // }
 
-  /**
-   *
-   * @param products
-   * @param sortOption URL query : 주어지지 않은 경우 products 그대로 반환
-   * @returns 주어진 정렬 옵션에 따라 정렬된 상태의 상품 목록 반환
-   *
-   * @throws 주어진 정렬 옵션이 SORT_OPTIONS에 존재하지 않으면 에러 발생
-   */
-  private async sortProducts(
-    products: Product[],
-    sortOption: string | undefined
-  ): Promise<Product[]> {
-    if (sortOption === undefined) return products;
-    if (!SORT_OPTIONS.includes(sortOption)) throw ERROR_TYPE.INVALID_SORT;
+  // /**
+  //  *
+  //  * @param products
+  //  * @param sortOption URL query : 주어지지 않은 경우 products 그대로 반환
+  //  * @returns 주어진 정렬 옵션에 따라 정렬된 상태의 상품 목록 반환
+  //  *
+  //  * @throws 주어진 정렬 옵션이 SORT_OPTIONS에 존재하지 않으면 에러 발생
+  //  */
+  // private async sortProducts(
+  //   products: Product[],
+  //   sortOption: string | undefined
+  // ): Promise<Product[]> {
+  //   if (sortOption === undefined) return products;
+  //   if (!SORT_OPTIONS.includes(sortOption)) throw ERROR_TYPE.INVALID_SORT;
 
-    switch (sortOption) {
-      case 'recommend':
-        return this.sortByReviewPoints(products);
-      case 'popularity':
-        return this.sortBySalesCount(products);
-      case 'recent':
-        return products;
-      case 'priceLow':
-        return [...products].sort((a, b) => a.price - b.price);
-      case 'priceHigh':
-        return [...products].sort((a, b) => b.price - a.price);
-      default:
-        return products;
-    }
-  }
+  //   switch (sortOption) {
+  //     case 'recommend':
+  //       return this.sortByReviewPoints(products);
+  //     case 'popularity':
+  //       return this.sortBySalesCount(products);
+  //     case 'recent':
+  //       return products;
+  //     case 'priceLow':
+  //       return [...products].sort((a, b) => a.price - b.price);
+  //     case 'priceHigh':
+  //       return [...products].sort((a, b) => b.price - a.price);
+  //     default:
+  //       return products;
+  //   }
+  // }
 
-  /**
-   *
-   * @param products
-   * @param pageNum URL query: 주어지지 않은 경우 기본값은 1
-   * @returns 주어진 페이지 번호에 맞는 상품 목록, 총 페이지 수 반환
-   *
-   * @throws 주어진 페이지 번호 형식이 올바르지 않은 경우 에러 발생
-   * @throws 주어진 페이지 번호가 총 페이지 수를 초과하면 에러 발생
-   */
-  private paginateProducts(products: Product[], pageNum: string | undefined) {
-    const page = pageNum === undefined ? 1 : +pageNum;
-    if (isNaN(page)) throw ERROR_TYPE.INVALID_PAGE;
+  // /**
+  //  *
+  //  * @param products
+  //  * @param pageNum URL query: 주어지지 않은 경우 기본값은 1
+  //  * @returns 주어진 페이지 번호에 맞는 상품 목록, 총 페이지 수 반환
+  //  *
+  //  * @throws 주어진 페이지 번호 형식이 올바르지 않은 경우 에러 발생
+  //  * @throws 주어진 페이지 번호가 총 페이지 수를 초과하면 에러 발생
+  //  */
+  // private paginateProducts(products: Product[], pageNum: string | undefined) {
+  //   const page = pageNum === undefined ? 1 : +pageNum;
+  //   if (isNaN(page)) throw ERROR_TYPE.INVALID_PAGE;
 
-    const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
-    if (page > totalPages) throw ERROR_TYPE.PAGE_OVERFLOW;
+  //   const totalPages = Math.ceil(products.length / ITEMS_PER_PAGE);
+  //   if (page > totalPages) throw ERROR_TYPE.PAGE_OVERFLOW;
 
-    const productsByPage = products.slice(ITEMS_PER_PAGE * (page - 1), ITEMS_PER_PAGE * page);
+  //   const productsByPage = products.slice(ITEMS_PER_PAGE * (page - 1), ITEMS_PER_PAGE * page);
 
-    return { products: productsByPage, totalPages };
-  }
+  //   return { products: productsByPage, totalPages };
+  // }
 
-  private async sortByReviewPoints(products: Product[]): Promise<Product[]> {
-    const reviewPoints = await Promise.all(
-      products.map(({ id }) => ReviewService.getAveragePointOfProduct(id))
-    );
+  // private async sortByReviewPoints(products: Product[]): Promise<Product[]> {
+  //   const reviewPoints = await Promise.all(
+  //     products.map(({ id }) => ReviewService.getAveragePointOfProduct(id))
+  //   );
 
-    const reviewPointsByProductId: { [key: string]: number } = {};
-    products.forEach(({ id }, index) => (reviewPointsByProductId[id] = reviewPoints[index]));
+  //   const reviewPointsByProductId: { [key: string]: number } = {};
+  //   products.forEach(({ id }, index) => (reviewPointsByProductId[id] = reviewPoints[index]));
 
-    return [...products].sort(
-      (a, b) => reviewPointsByProductId[b.id] - reviewPointsByProductId[a.id]
-    );
-  }
+  //   return [...products].sort(
+  //     (a, b) => reviewPointsByProductId[b.id] - reviewPointsByProductId[a.id]
+  //   );
+  // }
 
-  private async sortBySalesCount(products: Product[]): Promise<Product[]> {
-    const salesCounts = await Promise.all(
-      products.map(({ id }) => OrderDetailService.countTotalSalesOfProduct(id))
-    );
+  // private async sortBySalesCount(products: Product[]): Promise<Product[]> {
+  //   const salesCounts = await Promise.all(
+  //     products.map(({ id }) => OrderDetailService.countTotalSalesOfProduct(id))
+  //   );
 
-    const salesCountByProductId: { [key: string]: number } = {};
-    products.forEach(({ id }, index) => (salesCountByProductId[id] = salesCounts[index]));
+  //   const salesCountByProductId: { [key: string]: number } = {};
+  //   products.forEach(({ id }, index) => (salesCountByProductId[id] = salesCounts[index]));
 
-    return [...products].sort((a, b) => salesCountByProductId[b.id] - salesCountByProductId[a.id]);
-  }
+  //   return [...products].sort((a, b) => salesCountByProductId[b.id] - salesCountByProductId[a.id]);
+  // }
 }
 
 export default new ProductService();
