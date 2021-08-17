0daad4aef67b36c26cf1241aff21ef80b5514e33
momongss <rkrp15@gmail.com>
2021-08-17 18:30:11 +0900

    add: App 컴포넌트 추가

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/Header/Header';
-import Login from './components/login/Login';
-import { Router, Route } from './lib/router';
 
-ReactDOM.render(
-  <Router>
-    <Header />
-    <Route path="/login">
-      <Login />
-    </Route>
-  </Router>,
-  document.querySelector('#root')
-);
+import App from './components/App/App';
+
+ReactDOM.render(<App />, document.querySelector('#root'));
