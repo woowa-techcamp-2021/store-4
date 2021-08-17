0fba9d3e24a3f826994f70d5102427f37072f44d
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-18 00:16:48 +0900

    add: login test 파일 수정

--- client/src/components/login/Login.test.tsx
+++ client/src/components/login/Login.test.tsx
@@ -1,19 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import Login from './Login';
-
 import provideTheme2Test from '../../lib/provideTheme2Test';
+import { OAuthType } from '../../containers/LoginContainer';
 
 describe('Login 컴포넌트', () => {
-  test('facebook 버튼 UI', () => {
-    const login = render(provideTheme2Test(<Login />));
+  const FB_LOGIN_CONTENT = '페이스북';
+  const GOOGLE_LOGIN_CONTENT = '구글';
+  const BUTTONS = {
+    [OAuthType.Facebook]: {
+      backgroundColor: '#4267b2',
+      fontColor: '#ffffff',
+      icon: '',
+      content: FB_LOGIN_CONTENT,
+      redirectURL: `${process.env.SERVER_URL}/auth/facebook-login`,
+    },
+    [OAuthType.Google]: {
+      backgroundColor: '#EA4335',
+      fontColor: '#ffffff',
+      icon: '',
+      content: GOOGLE_LOGIN_CONTENT,
+      redirectURL: `${process.env.SERVER_URL}/auth/google-login`,
+    },
+  };
+  const handleOAuthButtonClick = jest.fn();
 
-    login.getByText('페이스북으로 로그인');
+  beforeEach(() => {
+    render(
+      provideTheme2Test(<Login buttons={BUTTONS} onOAuthButtonClick={handleOAuthButtonClick} />)
+    );
   });
 
-  test('google 버튼 UI', () => {
-    const login = render(provideTheme2Test(<Login />));
-
-    login.getByText('구글로 로그인');
+  test(`렌더 시 onOAuthButtonClick가 ${Object.entries(BUTTONS).length}번 불러와져야한다.`, () => {
+    expect(handleOAuthButtonClick).toBeCalledTimes(Object.entries(BUTTONS).length);
   });
 });
