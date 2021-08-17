0bef5093a5868202a71dfa5bec63a6f69c503b0d
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-18 00:16:05 +0900

    add: container, component 분리

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,13 +1,7 @@
-import React, { useCallback, useMemo } from 'react';
+import React from 'react';
 import styled from 'styled-components';
+import { OAuthType } from '../../containers/LoginContainer';
 import OAuthButton from './OAuthButton';
-import FACEBOOK from './facebook.svg';
-import GOOGLE from './google.svg';
-
-enum OAuthType {
-  Facebook,
-  Google,
-}
 
 const Header = styled.h1`
   font-size: ${(props) => props.theme.fontSize.large};
@@ -30,37 +24,34 @@ const OAuthButtonWrapper = styled.div`
   gap: 10px;
 `;
 
-const Login = (): JSX.Element => {
-  const handleOAuthButtonClick = useCallback((redirectURL: string) => {
-    return () => (location.href = redirectURL);
-  }, []);
+type Props = {
+  onOAuthButtonClick: (redirectURL: string) => React.MouseEventHandler;
+  buttons: {
+    [key: string]: {
+      backgroundColor: string;
+      fontColor: string;
+      icon: string;
+      content: string;
+      redirectURL: string;
+    };
+  };
+};
 
-  const BUTTON_PROPS = useMemo(
-    () => ({
-      [OAuthType.Facebook]: {
-        backgroundColor: '#4267b2',
-        fontColor: '#ffffff',
-        icon: FACEBOOK,
-        content: '페이스북으로 로그인',
-        onClick: handleOAuthButtonClick(`${process.env.SERVER_URL}/auth/facebook-login`),
-      },
-      [OAuthType.Google]: {
-        backgroundColor: '#EA4335',
-        fontColor: '#ffffff',
-        icon: GOOGLE,
-        content: '구글로 로그인',
-        onClick: handleOAuthButtonClick(`${process.env.SERVER_URL}/auth/google-login`),
-      },
-    }),
-    [handleOAuthButtonClick]
-  );
+const Login = (props: Props): JSX.Element => {
+  const { onOAuthButtonClick, buttons } = props;
 
   return (
     <LoginContainer>
       <Header>회원 로그인</Header>
       <OAuthButtonWrapper>
-        <OAuthButton {...BUTTON_PROPS[OAuthType.Facebook]} />
-        <OAuthButton {...BUTTON_PROPS[OAuthType.Google]} />
+        <OAuthButton
+          onClick={onOAuthButtonClick(buttons[OAuthType.Facebook].redirectURL)}
+          {...buttons[OAuthType.Facebook]}
+        />
+        <OAuthButton
+          onClick={onOAuthButtonClick(buttons[OAuthType.Google].redirectURL)}
+          {...buttons[OAuthType.Google]}
+        />
       </OAuthButtonWrapper>
     </LoginContainer>
   );
