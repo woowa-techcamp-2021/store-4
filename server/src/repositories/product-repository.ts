143215bf1758425c63cf7f47fdc06e93269749f8
sanginchun <sanginchun91@gmail.com>
2021-08-19 01:34:30 +0900

    chore: 더미데이터 및 관련 코드 제거

--- server/src/repositories/product-repository.ts
+++ server/src/repositories/product-repository.ts
@@ -1,30 +1,9 @@
-import { EntityRepository, getCustomRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository } from 'typeorm';
 import Product from '../models/product';
-import { ProductData } from '../dummy-data/product';
-import CategoryRepository from './category-repository';
 import { SortOption } from '../controllers/product-controller';
 
 @EntityRepository(Product)
 class ProductRepository extends Repository<Product> {
-  async createProducts(productData: Array<ProductData>): Promise<Product[]> {
-    const categories = await Promise.all(
-      productData.map(({ categoryId }) =>
-        getCustomRepository(CategoryRepository).findOne(categoryId)
-      )
-    );
-
-    const products = this.create(
-      productData.map(({ name, price, content }, index) => ({
-        name,
-        price,
-        content,
-        category: categories[index],
-      }))
-    );
-
-    return this.save(products);
-  }
-
   findProducts(
     categoryId: number,
     sortOption: SortOption,
