990ea5732dfa6548f691f51ca99ffd719df8a6d3
sanginchun <sanginchun91@gmail.com>
2021-08-18 18:02:15 +0900

    add: a 태그 reset 글로벌 스타일에 추가

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -10,6 +10,11 @@ const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  a {
+    text-decoration: none;
+    color: inherit;
+  }
 `;
 
 export default GlobalStyle;
