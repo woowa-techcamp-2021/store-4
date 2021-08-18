7dd5bddd327aa991391858aed6705bf08e5b68ec
sanginchun <sanginchun91@gmail.com>
2021-08-18 17:53:32 +0900

    fix: NavBar 컨벤션에 맞게 수정 - eslint warning 삭제를 위한 주석 추가 - 삼항연산자 -> &&로 변경

--- client/src/components/Header/NavBar/NavBar.tsx
+++ client/src/components/Header/NavBar/NavBar.tsx
@@ -2,24 +2,25 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Link } from '../../../lib/router';
 
+const CART_PATH = 'cart';
 const NAV_ITEMS = [
   { text: '로그인', path: 'login' },
   { text: '회원가입', path: 'signup' },
   { text: '마이페이지', path: 'my' },
-  { text: '장바구니', path: 'cart' },
+  { text: '장바구니', path: CART_PATH },
 ];
 
-const Wrapper = styled.div`
+const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   width: 100%;
   height: 40px;
-  border-bottom: 1px solid #eaeaea;
+  border-bottom: 1px solid ${(props) => props.theme.color.grey1};
 `;
 
 const Nav = styled.nav`
-  width: 1200px;
+  width: ${(props) => props.theme.device.desktop};
   height: 100%;
   display: flex;
   justify-content: flex-end;
@@ -29,55 +30,49 @@ const NavList = styled.ul`
   list-style: none;
   display: flex;
   align-items: center;
-  font-size: 12px;
+  font-size: ${(props) => props.theme.fontSize.tiny};
 `;
 
 const NavListItem = styled.li`
-  color: #717171;
+  color: ${(props) => props.theme.color.grey4};
+  padding: 0 10px;
+`;
 
-  a {
-    text-decoration: none;
-    color: inherit;
-    padding: 0 10px;
-  }
+const Badge = styled.span`
+  font-size: 10px;
+  line-height: 11px;
+  color: ${(props) => props.theme.color.mint2};
+  padding: 0 2px;
+`;
 
-  .seperator {
-    display: inline-block;
-    width: 1px;
-    height: 7px;
-    background-color: #ddd;
-  }
-
-  :last-child .seperator {
-    display: none;
-  }
-
-  .badge {
-    font-size: 10px;
-    line-height: 11px;
-    color: #29c1bc;
-    padding: 0 2px;
-  }
+const Seperator = styled.span`
+  display: inline-block;
+  width: 1px;
+  height: 7px;
+  background-color: ${(props) => props.theme.color.grey2};
 `;
 
 const NavBar = (): JSX.Element => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [cartItemCount, setCartItemCount] = useState(0);
 
-  const NavListItems = NAV_ITEMS.map((item, index) => (
-    <NavListItem key={index}>
-      <Link to={item.path}>
-        {item.text} {item.path === 'cart' ? <span className="badge">{cartItemCount}</span> : null}
-      </Link>
-      <span className="seperator"></span>
-    </NavListItem>
+  const NavListContent = NAV_ITEMS.map((item, index) => (
+    <React.Fragment key={index}>
+      <NavListItem>
+        <Link to={item.path}>
+          {item.text} {item.path === CART_PATH && <Badge>{cartItemCount}</Badge>}
+        </Link>
+      </NavListItem>
+      {index !== NAV_ITEMS.length - 1 && <Seperator />}
+    </React.Fragment>
   ));
 
   return (
-    <Wrapper>
+    <Container>
       <Nav>
-        <NavList>{NavListItems}</NavList>
+        <NavList>{NavListContent}</NavList>
       </Nav>
-    </Wrapper>
+    </Container>
   );
 };
 
