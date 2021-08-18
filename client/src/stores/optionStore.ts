af3205e54a3c9422d6bca55643f9e5ca0c7a6d06
sanginchun <sanginchun91@gmail.com>
2021-08-18 20:32:15 +0900

    add: optionStore 정의

---
+++ client/src/stores/optionStore.ts
@@ -0,0 +1,39 @@
+import { action, makeAutoObservable, observable } from 'mobx';
+import { Option } from '../types/option';
+import { ProductListOrder } from '../types/product';
+
+const DEFAULT_OPTION: Option = {
+  sortOption: ProductListOrder.Recommend,
+  pageNum: 1,
+};
+
+class OptionStore {
+  @observable
+  option: Option = DEFAULT_OPTION;
+
+  constructor() {
+    makeAutoObservable(this);
+  }
+
+  @action
+  setCategory(categoryId: number) {
+    this.option.categoryId = categoryId;
+  }
+
+  @action
+  setSortOption(sortOption: ProductListOrder) {
+    this.option.sortOption = sortOption;
+  }
+
+  @action
+  setPageNum(pageNum: number) {
+    this.option.pageNum = pageNum;
+  }
+
+  @action
+  setSearchTerm(searchTerm: string) {
+    this.option.searchTerm = searchTerm;
+  }
+}
+
+export default new OptionStore();
