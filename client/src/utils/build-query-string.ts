873507396066c4a61d775266c40d79b7c52a22c7
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-19 02:35:05 +0900

    add: buildQueryString에 key값에 대한 value가 없는 경우 ''을 반환

--- client/src/utils/build-query-string.ts
+++ client/src/utils/build-query-string.ts
@@ -2,7 +2,7 @@ type QueryObject = { [key: string]: string | number | null };
 
 export default (query: QueryObject): string => {
   const querystring = Object.keys(query)
-    .map((key) => `${key}=${query[key]}`)
+    .map((key) => `${key}=${query[key] || ''}`)
     .join('&');
 
   return `?${querystring}`;
