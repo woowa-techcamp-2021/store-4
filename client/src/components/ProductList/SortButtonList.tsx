0b2cf576ac04695774229d0407b6fbd66ed18b84
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-19 02:32:04 +0900

    add: productlist에서  sortbutton들을 모아서 list로 분리

---
+++ client/src/components/ProductList/SortButtonList.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import styled from 'styled-components';
+import { SortButton } from '../../../containers/ProductListContainer';
+import { ProductListOrder } from '../../../types/product';
+
+const Container = styled.div``;
+
+const Button = styled.button``;
+
+type Props = {
+  buttons: SortButton[];
+  onClickSortButton: (option: ProductListOrder) => () => void;
+};
+
+const SortButtonList = (props: Props): JSX.Element => {
+  const { buttons, onClickSortButton } = props;
+
+  const SortButtons = buttons.map((button) => (
+    <Button key={button.key} onClick={onClickSortButton(button.key)}>
+      {button.body}
+    </Button>
+  ));
+
+  return <Container>{SortButtons}</Container>;
+};
+
+export default SortButtonList;
