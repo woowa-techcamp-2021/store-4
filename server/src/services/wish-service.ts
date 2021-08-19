c100c0597d68577e7ded82e5bf2b16abc62e0c2d
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-20 02:29:53 +0900

    add: wishService deleteWish 작성

--- server/src/services/wish-service.ts
+++ server/src/services/wish-service.ts
@@ -7,6 +7,12 @@ import UserRepository from '../repositories/user-repository';
 import WishRepository from '../repositories/wish-repository';
 import { isNone } from '../util/type-guard';
 
+type DeleteWishArgs = {
+  wishId: number;
+  userId: number;
+  productId: number;
+};
+
 class WishService {
   async insertWish(userId: number, productId: number): Promise<Wish> {
     const user = await getCustomRepository(UserRepository).findOne(userId);
@@ -19,6 +25,19 @@ class WishService {
     }
 
     return getCustomRepository(WishRepository).save({ user, product });
+  }
+
+  async deleteWish({ wishId, userId, productId }: DeleteWishArgs): Promise<Wish> {
+    const user = await getCustomRepository(UserRepository).findOne(userId);
+    if (isNone(user)) {
+      throw new UserNotfoundException('유저가 없습니다.');
+    }
+    const product = await getCustomRepository(ProductRepository).findOne(productId);
+    if (isNone(product)) {
+      throw new ProductNotfoundException('상품이 존재하지 않습니다.');
+    }
+
+    return getCustomRepository(WishRepository).remove({ id: wishId, user, product });
   }
 }
 
