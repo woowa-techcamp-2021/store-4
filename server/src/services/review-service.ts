ae39d75b4d5b5c06041f0a538501ddbca5c14205
sanginchun <sanginchun91@gmail.com>
2021-08-19 17:48:06 +0900

    chore: 불필요해진 service, repository 메소드 삭제

--- server/src/services/review-service.ts
+++ server/src/services/review-service.ts
@@ -1,17 +1,3 @@
-import { getCustomRepository } from 'typeorm';
-import ReviewRepository from '../repositories/review-repository';
-
-const DECIMAL_POINTS = 1;
-const DEFAULT_POINT = 3;
-
-class ReviewService {
-  async getAveragePointOfProduct(productId: number): Promise<number> {
-    const reviews = await getCustomRepository(ReviewRepository).findByProduct(productId);
-    if (reviews.length === 0) return DEFAULT_POINT;
-
-    const sum = reviews.map((review) => review.point).reduce((acc, val) => acc + val, 0);
-    return parseFloat((sum / reviews.length).toFixed(DECIMAL_POINTS));
-  }
-}
+class ReviewService {}
 
 export default new ReviewService();
