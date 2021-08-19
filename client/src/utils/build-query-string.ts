ae215ea8e0facac510594d9084330f8937d73f8c
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-19 19:59:11 +0900

    add: build-query-string value가 없는 경우 query로 안만들어줌

--- client/src/utils/build-query-string.ts
+++ client/src/utils/build-query-string.ts
@@ -2,6 +2,7 @@ type QueryObject = { [key: string]: string | number | null };
 
 export default (query: QueryObject): string => {
   const querystring = Object.keys(query)
+    .filter((key) => query[key])
     .map((key) => `${key}=${query[key] || ''}`)
     .join('&');
 
