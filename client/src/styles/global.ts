58c1bd21df3a4c9d14bd6d2ea179aa4a8bd818dd
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-19 21:10:48 +0900

    fix: Global 스타일 폰트 및 기본 색 변경

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -4,11 +4,12 @@ import { normalize } from 'styled-normalize';
 const GlobalStyle = createGlobalStyle`
   ${normalize}
   * {
-    font-family: 'Nanum Gothic', 'Noto Sans KR', -apple-system, system-ui, BlinkMacSystemFont,
+    font-family: 'Noto Sans KR', -apple-system, system-ui, BlinkMacSystemFont,
         'Segoe UI', sans-serif;
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    color: ${(props) => props.theme.color.black};
   }
 
   a {
