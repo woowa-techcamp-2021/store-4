2f37dce142b717167df7526dc3fde3a4b8a475e4
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-19 21:45:35 +0900

    add: SortButton에 현재 선택된 sort옵션에 따라서 하이라이팅

--- client/src/components/ProductList/SortButtonList.tsx
+++ client/src/components/ProductList/SortButtonList.tsx
@@ -1,11 +1,30 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 import styled from 'styled-components';
 import { SortButton } from '../../containers/ProductListContainer';
+import optionStore from '../../stores/optionStore';
 import { ProductListOrder } from '../../types/product';
 
 const Container = styled.div``;
 
-const Button = styled.button``;
+type ButtonProps = {
+  isSelected: boolean;
+};
+
+const Button = styled.button<ButtonProps>`
+  font-size: ${(props) => props.theme.fontSize.small};
+  font-weight: 700;
+  margin: 0px 24px;
+  background-color: ${(props) => props.theme.color.white1};
+  color: ${(props) => (props.isSelected ? props.theme.color.mint2 : props.theme.color.black)};
+  border: none;
+  &:hover {
+    cursor: pointer;
+  }
+  &:last-child {
+    margin-right: 0px;
+  }
+`;
 
 type Props = {
   buttons: SortButton[];
@@ -14,9 +33,14 @@ type Props = {
 
 const SortButtonList = (props: Props): JSX.Element => {
   const { buttons, onClickSortButton } = props;
+  const option = optionStore.option;
 
   const SortButtons = buttons.map((button) => (
-    <Button key={button.key} onClick={onClickSortButton(button.key)}>
+    <Button
+      key={button.key}
+      onClick={onClickSortButton(button.key)}
+      isSelected={option.sort === button.key}
+    >
       {button.body}
     </Button>
   ));
@@ -24,4 +48,4 @@ const SortButtonList = (props: Props): JSX.Element => {
   return <Container>{SortButtons}</Container>;
 };
 
-export default SortButtonList;
+export default observer(SortButtonList);
