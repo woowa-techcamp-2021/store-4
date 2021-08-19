4173b1e4132c935c79e9a11c9f450d66a920a437
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-19 17:23:56 +0900

    add: test 코드 작성

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
-    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg|gif)$': 'jest-transform-stub',
   },
 };
