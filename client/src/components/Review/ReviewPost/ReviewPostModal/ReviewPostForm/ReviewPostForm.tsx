478b93851bf0343d06a8e4bf14cec65271a9fdc2
sanginchun <sanginchun91@gmail.com>
2021-08-21 02:07:22 +0900

    add: 리뷰 쓰기 Form에 styled-components 적용

--- client/src/components/Review/ReviewPost/ReviewPostModal/ReviewPostForm/ReviewPostForm.tsx
+++ client/src/components/Review/ReviewPost/ReviewPostModal/ReviewPostForm/ReviewPostForm.tsx
@@ -1,4 +1,26 @@
 import React, { MouseEventHandler } from 'react';
+import styled from 'styled-components';
+
+const FORM_TITLE_TEXT = '상품후기';
+const FORM_CANCEL_BUTTON_TEXT = '취소';
+const FORM_SUBMIT_BUTTON_TEXT = '리뷰 남기기';
+
+const Container = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const FormTitle = styled.h1`
+  font-size: ${(props) => props.theme.fontSize.medium};
+`;
+
+const FormInputFile = styled.input``;
+
+const FormInputTextarea = styled.textarea``;
+
+const FormCancelButton = styled.button``;
+
+const FormSubmitButton = styled.button``;
 
 type Props = {
   onCancelButtonClick: MouseEventHandler;
@@ -6,12 +28,13 @@ type Props = {
 const ReviewPostForm = (props: Props): JSX.Element => {
   const { onCancelButtonClick } = props;
   return (
-    <form>
-      <h1>글쓰기</h1>
-      <input type="text" />
-      <input type="file" />
-      <button onClick={onCancelButtonClick}>닫기</button>
-    </form>
+    <Container>
+      <FormTitle>{FORM_TITLE_TEXT}</FormTitle>
+      <FormInputFile type="file" multiple={true} />
+      <FormInputTextarea />
+      <FormCancelButton onClick={onCancelButtonClick}>{FORM_CANCEL_BUTTON_TEXT}</FormCancelButton>
+      <FormSubmitButton type="submit">{FORM_SUBMIT_BUTTON_TEXT}</FormSubmitButton>
+    </Container>
   );
 };
 
