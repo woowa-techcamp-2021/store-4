0329fb35cdf5d4eb994d2aeca13a9ba66da0b96c
sanginchun <sanginchun91@gmail.com>
2021-08-20 20:24:27 +0900

    chore: 아이콘 추가, 유틸 함수 삭제

--- client/src/utils/formatText.ts
+++
@@ -1,6 +0,0 @@
-export default (text: string, maxLength: number): string => {
-  let resultText = text.slice(0, maxLength);
-  if (text.length > maxLength) resultText += '...';
-
-  return resultText;
-};
