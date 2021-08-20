b27a187587695df7d780f0e1c5579864832b5c85
sanginchun <sanginchun91@gmail.com>
2021-08-20 20:22:51 +0900

    chore: 테스트를 위한 목데이터 수정

--- client/src/containers/ReviewContainer.tsx
+++ client/src/containers/ReviewContainer.tsx
@@ -12,14 +12,14 @@ const mockReviewData: Mock[] = [
   {
     id: 1,
     content:
-      '상당히 마음에 듭니다. 더 살지 고민이에요. 너무 좋아요 하하하하하. 고민 중이라면 그냥 사세요. 두 번 사세요. 제발 부탁입니다. 두 번 사세요. 제발 부탁입니다. 두 번 사세요. 제발 부탁입니다.',
+      '상당히 마음에 듭니다. 더 살지 고민이에요. 너무 좋아요 하하하하하. 고민 중이라면 그냥 사세요. 두 번 사세요. 세 번 사세요.',
     point: 5,
-    reviewImages: ['image-1'],
+    reviewImages: ['https://source.unsplash.com/random/300x400'],
     updatedAt: new Date(),
   },
   {
     id: 2,
-    content: '별로에요',
+    content: '별로에요 별로에요 별로에요 별로에요',
     point: 2,
     reviewImages: [],
     updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24),
@@ -28,14 +28,17 @@ const mockReviewData: Mock[] = [
     id: 3,
     content: '좋아요',
     point: 4,
-    reviewImages: ['image-2'],
+    reviewImages: ['https://source.unsplash.com/random/700x500'],
     updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 2),
   },
   {
     id: 4,
     content: '그냥저냥',
     point: 3,
-    reviewImages: ['image-3', 'image-4'],
+    reviewImages: [
+      'https://source.unsplash.com/random/600x400',
+      'https://source.unsplash.com/random/400x300',
+    ],
     updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 3),
   },
 ];
