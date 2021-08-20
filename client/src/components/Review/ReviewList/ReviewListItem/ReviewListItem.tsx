3d3e87ad99a00887e1b865b2df358d92c76a8f02
sanginchun <sanginchun91@gmail.com>
2021-08-20 20:23:44 +0900

    add: ReviewListItem 컴포넌트

--- client/src/components/Review/ReviewList/ReviewListItem/ReviewListItem.tsx
+++ client/src/components/Review/ReviewList/ReviewListItem/ReviewListItem.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useCallback, useLayoutEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { Mock } from '../../../../containers/ReviewContainer';
 import formatDate from '../../../../utils/formatDate';
-import formatText from '../../../../utils/formatText';
+import ReviewDetail from './ReviewDetail/ReviewDetail';
 import { generateStars } from './Star';
+import CHEVRON_DOWN from './chevronDown.png';
 
-const MAX_CONTENT_LENGTH = 80;
+const MAX_TITLE_WIDTH = 700;
 
 const Container = styled.li`
-  display: flex;
-  align-items: center;
   padding: 10px 20px;
   border-bottom: 1px solid ${(props) => props.theme.color.grey2};
 
@@ -18,21 +17,58 @@ const Container = styled.li`
   }
 `;
 
-const ReviewStars = styled.span`
+const ReviewDisplayContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const ReviewStars = styled.div`
   flex-shrink: 0;
   width: 80px;
   margin-right: 40px;
   display: flex;
 `;
 
-const ReviewTitle = styled.span`
-  flex: 1;
-  font-size: ${(props) => props.theme.fontSize.small};
+type ClickContainerProps = {
+  isClickable: boolean;
+};
+const ClickContainer = styled.div<ClickContainerProps>`
+  flex-grow: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  cursor: ${(props) => (props.isClickable ? 'pointer' : 'default')};
 `;
 
-const ReviewDate = styled.span`
+const ReviewTitle = styled.span`
+  font-size: ${(props) => props.theme.fontSize.small};
+  max-width: ${MAX_TITLE_WIDTH}px;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
+
+type SeeMoreProps = {
+  isClose: boolean;
+};
+const SeeMore = styled.button<SeeMoreProps>`
+  width: 10px;
+  height: 10px;
+  margin-right: 16px;
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+  background-image: url(${CHEVRON_DOWN});
+  background-size: contain;
+  transform: rotate(${(props) => (props.isClose ? '-180' : '0')}deg);
+  transition: transform 0.3s ease-in-out;
+`;
+
+const ReviewDate = styled.div`
+  flex-shrink: 0;
+  width: 100px;
   font-size: ${(props) => props.theme.fontSize.tiny};
-  text-align: left;
+  text-align: right;
 `;
 
 type Props = {
@@ -41,13 +77,32 @@ type Props = {
 
 const ReviewListItem = (props: Props): JSX.Element => {
   const { review } = props;
+  const [reviewDetailOpen, setReviewDetailOpen] = useState(false);
+  const titleRef = useRef<HTMLHeadingElement>(null);
+  const [hasMoreContent, setHasMoreContent] = useState(review.reviewImages.length > 0);
   const stars = generateStars(review.point);
+
+  useLayoutEffect(() => {
+    const isTitleOverflowed = titleRef.current?.clientWidth === MAX_TITLE_WIDTH;
+    setHasMoreContent((prevState) => prevState || isTitleOverflowed);
+  }, []);
+
+  const handleReviewItemClick = useCallback(
+    () => setReviewDetailOpen((prev) => !prev),
+    [setReviewDetailOpen]
+  );
 
   return (
     <Container>
-      <ReviewStars>{stars}</ReviewStars>
-      <ReviewTitle>{formatText(review.content, MAX_CONTENT_LENGTH)}</ReviewTitle>
-      <ReviewDate>{formatDate(review.updatedAt)}</ReviewDate>
+      <ReviewDisplayContainer>
+        <ReviewStars>{stars}</ReviewStars>
+        <ClickContainer onClick={handleReviewItemClick} isClickable={hasMoreContent}>
+          <ReviewTitle ref={titleRef}>{review.content}</ReviewTitle>
+          {hasMoreContent && <SeeMore isClose={reviewDetailOpen} />}
+        </ClickContainer>
+        <ReviewDate>{formatDate(review.updatedAt)}</ReviewDate>
+      </ReviewDisplayContainer>
+      {reviewDetailOpen && hasMoreContent && <ReviewDetail review={review} />}
     </Container>
   );
 };
