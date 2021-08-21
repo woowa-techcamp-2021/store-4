b4c632df7556a39af97adde267c36861184c7ad7
sanginchun <sanginchun91@gmail.com>
2021-08-21 14:15:42 +0900

    add: 목데이터 추가

--- client/src/containers/ReviewContainer.tsx
+++ client/src/containers/ReviewContainer.tsx
@@ -41,6 +41,63 @@ const mockReviewData: Mock[] = [
     ],
     updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 3),
   },
+  {
+    id: 5,
+    content:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis sed tempora aspernatur sequi molestias nulla voluptatum iusto impedit error consectetur?',
+    point: 3,
+    reviewImages: [
+      'https://source.unsplash.com/random/600x400',
+      'https://source.unsplash.com/random/400x300',
+    ],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 3),
+  },
+  {
+    id: 6,
+    content:
+      'r sit, amet consectetur adipisicing elit. Facilis sed tempora aspernatur sequi molestias nulla voluptatum',
+    point: 3,
+    reviewImages: [
+      'https://source.unsplash.com/random/600x500',
+      'https://source.unsplash.com/random/500x400',
+    ],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 4),
+  },
+  {
+    id: 7,
+    content:
+      'um dolor sit, amet consectetur adipisicing elit. Facilis sed tempora aspernatur sequi molestias nulla voluptatum iusto impedit error consect',
+    point: 4,
+    reviewImages: [
+      'https://source.unsplash.com/random/600x400',
+      'https://source.unsplash.com/random/400x300',
+    ],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 4.5),
+  },
+  {
+    id: 8,
+    content: 'olor sit, amet consecte',
+    point: 4,
+    reviewImages: [],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 7),
+  },
+  {
+    id: 9,
+    content: 'amet consectetur adipisicing elit. Facilis sed tempora aspernatur sequi molestias ',
+    point: 2,
+    reviewImages: [
+      'https://source.unsplash.com/random/600x400',
+      'https://source.unsplash.com/random/400x300',
+    ],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 30),
+  },
+  {
+    id: 10,
+    content: 'sed tempora aspernatur sequi molestias',
+    point: 5,
+    reviewImages: [],
+    updatedAt: new Date(Date.now() - 1000 * 60 * 60 * 24 * 50),
+  },
 ];
 
 const ReviewConatiner = (): JSX.Element => {
