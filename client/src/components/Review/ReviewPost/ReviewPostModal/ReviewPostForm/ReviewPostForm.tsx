5c2a6d22e88ea436420f67480e0b25960547d8d1
sanginchun <sanginchun91@gmail.com>
2021-08-21 14:06:29 +0900

    add: Review Form 컴포넌트 여러 하위 컴포넌트로 분리, 상수 선언

--- client/src/components/Review/ReviewPost/ReviewPostModal/ReviewPostForm/ReviewPostForm.tsx
+++ client/src/components/Review/ReviewPost/ReviewPostModal/ReviewPostForm/ReviewPostForm.tsx
@@ -1,39 +1,66 @@
-import React, { MouseEventHandler } from 'react';
+import React, { ChangeEvent, MouseEventHandler, useCallback, useState } from 'react';
 import styled from 'styled-components';
+import ReviewPostStars from './ReviewPostStars/ReviewPostStars';
+import ReviewPostText from './ReviewPostText/ReviewPostText';
+import ReviewPostImages from './ReviewPostImages/ReviewPostImages';
+import ReviewPostButtonGroup from './ReviewPostButtonGroup/ReviewPostButtonGroup';
 
-const FORM_TITLE_TEXT = '상품후기';
-const FORM_CANCEL_BUTTON_TEXT = '취소';
-const FORM_SUBMIT_BUTTON_TEXT = '리뷰 남기기';
+const STARS_WIDTH = 300;
+const DEFAULT_TEXT_FOR_POINT: { [key: number]: string } = {
+  1: '매우 별로였어요',
+  2: '별로였어요',
+  3: '그냥 그랬어요',
+  4: '좋았어요',
+  5: '최고에요',
+};
+const ADD_IMAGE_BUTTON_TEXT = '사진 추가';
+const IMAGE_THUMBNAIL_SIZE = 90;
+const IMAGE_LIST_PLACEHOLDER_TEXT = '사진을 추가하려면 왼쪽 버튼을 클릭하세요';
+const CANCEL_BUTTON_TEXT = '취소';
+const SUBMIT_BUTTON_TEXT = '리뷰 남기기';
 
 const Container = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
-const FormTitle = styled.h1`
-  font-size: ${(props) => props.theme.fontSize.medium};
-`;
-
-const FormInputFile = styled.input``;
-
-const FormInputTextarea = styled.textarea``;
-
-const FormCancelButton = styled.button``;
-
-const FormSubmitButton = styled.button``;
+const ReviewPostStarsContainer = styled.div``;
 
 type Props = {
   onCancelButtonClick: MouseEventHandler;
 };
 const ReviewPostForm = (props: Props): JSX.Element => {
+  const [point, setPoint] = useState(0);
+  const [inputText, setInputText] = useState('');
+  const imageList: string[] = [];
   const { onCancelButtonClick } = props;
+  const handleStarClick = useCallback((point: number) => {
+    setPoint(point);
+    setInputText(DEFAULT_TEXT_FOR_POINT[point]);
+  }, []);
+  const handleTextChange = useCallback((event: ChangeEvent) => {
+    const target = event.target as HTMLTextAreaElement;
+    setInputText(target.value);
+  }, []);
+
   return (
     <Container>
-      <FormTitle>{FORM_TITLE_TEXT}</FormTitle>
-      <FormInputFile type="file" multiple={true} />
-      <FormInputTextarea />
-      <FormCancelButton onClick={onCancelButtonClick}>{FORM_CANCEL_BUTTON_TEXT}</FormCancelButton>
-      <FormSubmitButton type="submit">{FORM_SUBMIT_BUTTON_TEXT}</FormSubmitButton>
+      <ReviewPostStarsContainer>
+        <ReviewPostStars width={STARS_WIDTH} onStarClick={handleStarClick} />
+      </ReviewPostStarsContainer>
+      <ReviewPostText value={inputText} onChange={handleTextChange} />
+      <ReviewPostImages
+        addImageButtonText={ADD_IMAGE_BUTTON_TEXT}
+        imageThumbnailSize={IMAGE_THUMBNAIL_SIZE}
+        imageListPlaceholderText={IMAGE_LIST_PLACEHOLDER_TEXT}
+        images={imageList}
+      />
+      <ReviewPostButtonGroup
+        cancelButtonText={CANCEL_BUTTON_TEXT}
+        submitButtonText={SUBMIT_BUTTON_TEXT}
+        onCancelButtonClick={onCancelButtonClick}
+        isSubmitDisabled={point === 0}
+      />
     </Container>
   );
 };
