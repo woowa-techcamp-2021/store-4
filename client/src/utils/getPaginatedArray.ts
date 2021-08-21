3f0e146965ab4772ffcd221c28a6e7e1bc3cbea9
sanginchun <sanginchun91@gmail.com>
2021-08-22 01:36:27 +0900

    fix: if문에 {} 사용

--- client/src/utils/getPaginatedArray.ts
+++ client/src/utils/getPaginatedArray.ts
@@ -2,7 +2,9 @@ export default <T>(arr: T[], itemsPerPage: number, currentPage: number): T[] => 
   if (arr.length === 0) return arr.slice();
 
   const totalPages = Math.ceil(arr.length / itemsPerPage);
-  if (currentPage > totalPages) throw new Error('Page Overflow');
+  if (currentPage > totalPages) {
+    throw new Error('Page Overflow');
+  }
 
   return arr.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage);
 };
