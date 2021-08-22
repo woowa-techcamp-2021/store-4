94ea572ce19db9ad80cf1489bca668e5c345949a
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-22 12:02:13 +0900

    chore: 네이밍 수정

--- client/src/components/MainProductSections/mock/index.ts
+++ client/src/components/MainProductSections/mock/index.ts
@@ -1,13 +1,13 @@
-import Science from './hyoja_pc.png';
-import FakeSet from './pc_kkfakeset.png';
+import SCIENCE from './hyoja_pc.png';
+import FAKESET from './pc_kkfakeset.png';
 import { ProductItemType } from '../../../types/product';
 
 export type MockProductItemType = ProductItemType & { discountRate: number };
 export type MockProductAdItemType = { id: number; title: string; subTitle: string; imgSrc: string };
 
 const adMockData = [
-  { id: 1, title: '효자손은 과학이다', subTitle: '시원하게 긁어드려요', imgSrc: Science },
-  { id: 2, title: '꼭꼭 숨어라', subTitle: 'ㅋㅋ안 보이는 양말세트', imgSrc: FakeSet },
+  { id: 1, title: '효자손은 과학이다', subTitle: '시원하게 긁어드려요', imgSrc: SCIENCE },
+  { id: 2, title: '꼭꼭 숨어라', subTitle: 'ㅋㅋ안 보이는 양말세트', imgSrc: FAKESET },
 ];
 
 export const getAdProductList = (): Promise<MockProductAdItemType[]> => {
