ed3b90cf25745813f1efbdd9ce5fe7ccb99f79e5
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-22 21:49:48 +0900

    add: DeliveryAddress 수정 폼으로 이동 버튼 제작

--- client/src/components/DeliveryAddress/DeliveryAddressItem.tsx
+++ client/src/components/DeliveryAddress/DeliveryAddressItem.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 import styled from 'styled-components';
 import DeliveryAddress from '../../models/delivery-address';
 
 const Container = styled.div`
   width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 150px;
+  padding: 0px 20px;
 `;
+
+const DeliveryAddressWrapper = styled.div``;
 
 const RowWrapper = styled.div`
   color: ${(props) => props.theme.color.grey5};
@@ -12,11 +19,13 @@ const RowWrapper = styled.div`
   align-items: center;
   margin: 10px 0;
   gap: 8px;
+  padding: 5px 0px;
 `;
 
 const DeliveryAddressNickName = styled.div`
   font-size: ${(props) => props.theme.fontSize.medium};
   font-weight: bold;
+  padding: 5px 0px;
 `;
 
 const Address = styled.div``;
@@ -25,25 +34,40 @@ const RecipientName = styled.div``;
 
 const RecipientPhoneNumber = styled.div``;
 
+const ChangeToModifyButton = styled.button`
+  border: 1px solid ${(props) => props.theme.color.grey3};
+  background-color: ${(props) => props.theme.color.grey1};
+  color: ${(props) => props.theme.color.grey5};
+  font-size: ${(props) => props.theme.fontSize.small};
+  width: 80px;
+  height: 30px;
+  outline: none;
+  cursor: pointer;
+`;
+
 type Props = {
   deliveryAddress: DeliveryAddress;
+  onToModifyClick: MouseEventHandler;
 };
 
 const DeliveryAddressItem = (props: Props): JSX.Element => {
-  const { deliveryAddress } = props;
+  const { deliveryAddress, onToModifyClick } = props;
   const { name, recipientName, address, recipientPhoneNumber } = deliveryAddress;
 
   return (
     <Container>
-      <DeliveryAddressNickName>{name}</DeliveryAddressNickName>
-      <RowWrapper>
-        <Address>{address}</Address>
-      </RowWrapper>
+      <DeliveryAddressWrapper>
+        <DeliveryAddressNickName>{name}</DeliveryAddressNickName>
+        <RowWrapper>
+          <Address>{address}</Address>
+        </RowWrapper>
 
-      <RowWrapper>
-        <RecipientName>({recipientName})</RecipientName>
-        <RecipientPhoneNumber>{recipientPhoneNumber}</RecipientPhoneNumber>
-      </RowWrapper>
+        <RowWrapper>
+          <RecipientName>({recipientName})</RecipientName>
+          <RecipientPhoneNumber>{recipientPhoneNumber}</RecipientPhoneNumber>
+        </RowWrapper>
+      </DeliveryAddressWrapper>
+      <ChangeToModifyButton onClick={onToModifyClick}>수정</ChangeToModifyButton>
     </Container>
   );
 };
