0d0b9a7efc684aab4dec7a55d1628d2776ce178e
sanginchun <sanginchun91@gmail.com>
2021-08-22 16:31:23 +0900

    chore: wrapAsync로 감싸기

--- server/src/routers/api/review/index.ts
+++ server/src/routers/api/review/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import reviewController from '../../../controllers/review-controller';
+import wrapAsync from '../../../lib/wrap-async';
 import authMiddleware from '../../../middlewares/auth-middleware';
 
 const reviewRouter = Router();
 
-reviewRouter.post('/', authMiddleware('user'), reviewController.post);
+reviewRouter.post('/', authMiddleware('user'), wrapAsync(reviewController.post));
 
 export default reviewRouter;
