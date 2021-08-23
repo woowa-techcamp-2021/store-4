de3e28367f75c28bd39579a98c650a0ba02f0c77
sanginchun <sanginchun91@gmail.com>
2021-08-23 23:57:12 +0900

    add: 로그아웃 로직 및 컴포넌트

---
+++ client/src/pages/Logout.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import LogoutContainer from '../containers/LogoutContainer';
+
+const LoginPage = (): JSX.Element => {
+  return <LogoutContainer />;
+};
+
+export default LoginPage;
