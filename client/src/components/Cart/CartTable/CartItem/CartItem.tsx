44221cddc5b157aefc1b2244f3b4b7f2bb96a6e3
momongss <rkrp15@gmail.com>
2021-08-24 01:17:26 +0900

    add: CartTable 옵션 이름 추가

--- client/src/components/Cart/CartTable/CartItem/CartItem.tsx
+++ client/src/components/Cart/CartTable/CartItem/CartItem.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import cartStore from '../../../../stores/cartStore';
+import { SelectWithSelected } from '../../../../types/product';
 import { toKoreanMoneyFormat } from '../../../../utils/moneyFormater';
 
 const AlignCenterContainer = styled.div`
@@ -20,9 +21,10 @@ const ResetButton = styled.button`
   outline: inherit;
 `;
 
-const CartItemWrapper = styled.div`
+const Container = styled.div`
   display: flex;
   align-items: center;
+  width: 100%;
   height: 82px;
   &:not(:first-child) {
     border-top: 1px solid ${(props) => props.theme.color.grey3};
@@ -32,7 +34,7 @@ const CartItemWrapper = styled.div`
 const ItemTitleWrapper = styled.div`
   display: flex;
   align-items: center;
-  width: 802px;
+  width: 100%;
   padding: 0 10px;
 `;
 
@@ -48,7 +50,17 @@ const ItemImg = styled.img`
   padding-right: 10px;
 `;
 
+const ItemWrapper = styled.div``;
+
 const ItemTitle = styled.div``;
+
+const Option = styled.div`
+  padding-top: 4px;
+
+  font-size: ${(props) => props.theme.fontSize.tiny};
+  font-weight: 400;
+  color: ${(props) => props.theme.color.grey3};
+`;
 
 const CountWrapper = styled(AlignCenterContainer)`
   flex-direction: column;
@@ -81,23 +93,30 @@ type Props = {
   count: number;
   price: number;
   isSelected: boolean;
+  selectWithSelected: SelectWithSelected;
 };
 
 const CartItem = (props: Props): JSX.Element => {
-  const { onOptionClick, id, title, imgSrc, count, price, isSelected } = props;
+  const { onOptionClick, id, title, imgSrc, count, price, isSelected, selectWithSelected } = props;
+  const optionPrice = selectWithSelected ? selectWithSelected.selectedOption.additionalPrice : 0;
+  const optionTypeName = selectWithSelected ? selectWithSelected.name : '';
+  const optionName = selectWithSelected ? selectWithSelected.selectedOption.name : '';
 
   const onChangeCheckBox = (e: React.ChangeEvent<HTMLInputElement>) => {
     cartStore.setCartItemSelection(id, e.target.checked);
   };
 
   return (
-    <CartItemWrapper>
+    <Container>
       <CheckBoxWrapper>
         <CheckBox type="checkbox" onChange={onChangeCheckBox} checked={isSelected} />
       </CheckBoxWrapper>
       <ItemTitleWrapper>
         <ItemImg src={imgSrc} />
-        <ItemTitle>{title}</ItemTitle>
+        <ItemWrapper>
+          <ItemTitle>{title}</ItemTitle>
+          <Option>{selectWithSelected ? `${optionTypeName} : ${optionName}` : ''}</Option>
+        </ItemWrapper>
       </ItemTitleWrapper>
       <CountWrapper>
         <Count>{count}개</Count>
@@ -110,9 +129,9 @@ const CartItem = (props: Props): JSX.Element => {
         </OptionChangeButton>
       </CountWrapper>
       <PriceWrapper>
-        <Price>{toKoreanMoneyFormat(price)}</Price>
+        <Price>{toKoreanMoneyFormat(price + optionPrice)}</Price>
       </PriceWrapper>
-    </CartItemWrapper>
+    </Container>
   );
 };
 
--- client/src/components/Cart/CartTable/CartTable.tsx
+++ client/src/components/Cart/CartTable/CartTable.tsx
@@ -5,9 +5,8 @@ import TableHeader from './TableHeader/TableHeader';
 import CartItem from './CartItem/CartItem';
 import cartStore from '../../../stores/cartStore';
 import { observer } from 'mobx-react';
-import { toKoreanMoneyFormat } from '../../../utils/moneyFormater';
 
-const CartTableWrapper = styled.div`
+const Container = styled.div`
   font-size: ${(props) => props.theme.fontSize.tiny};
   font-weight: 700;
   margin-top: 60px;
@@ -22,7 +21,8 @@ const TableMain = styled.div`
 const CartItemList = styled.div`
   display: flex;
   flex-direction: column;
-  border-right: 1px solid ${(props) => props.theme.color.grey3};
+
+  width: 100%;
 `;
 
 type Props = {
@@ -33,7 +33,7 @@ const CartTable = (props: Props): JSX.Element => {
   const { onOptionClick } = props;
 
   return (
-    <CartTableWrapper>
+    <Container>
       <TableHeader />
       <TableMain>
         <CartItemList>
@@ -46,12 +46,13 @@ const CartTable = (props: Props): JSX.Element => {
               count={item.count}
               price={item.price * item.count}
               isSelected={item.isSelected}
+              selectWithSelected={item.selectWithSelected}
               onOptionClick={onOptionClick}
             />
           ))}
         </CartItemList>
       </TableMain>
-    </CartTableWrapper>
+    </Container>
   );
 };
 
