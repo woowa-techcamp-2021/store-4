e874d537ebb0ec01bd17656d99ea9b5093e4ad95
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-23 23:00:34 +0900

    add: DeliveryAddress 생성 Service 메소드

--- server/src/services/delivery-address-service.ts
+++ server/src/services/delivery-address-service.ts
@@ -1,6 +1,10 @@
 import { getCustomRepository } from 'typeorm';
+import UserNotfoundException from '../exceptions/user-notfound-exception';
 import DeliveryAddress from '../models/delivery-address';
 import DeliveryAddressRepository from '../repositories/delivery-address-repository';
+import UserRepository from '../repositories/user-repository';
+import { isNone } from '../util/type-guard';
+import DeliveryAddressCreate from '../validations/delivery-address-create';
 
 class DeliveryAddressService {
   async findAll(userId: number): Promise<DeliveryAddress[]> {
@@ -9,6 +13,21 @@ class DeliveryAddressService {
     );
 
     return deliveryAddresses;
+  }
+
+  async create(
+    userId: number,
+    deliveryAddressCreate: DeliveryAddressCreate
+  ): Promise<DeliveryAddress> {
+    const user = await getCustomRepository(UserRepository).findOne(userId);
+    if (isNone(user)) {
+      throw new UserNotfoundException('유저가 없습니다.');
+    }
+
+    return getCustomRepository(DeliveryAddressRepository).save({
+      ...deliveryAddressCreate,
+      user,
+    });
   }
 }
 
