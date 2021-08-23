4683c7b6d6cea13026fefc842adff957207b1f8d
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-23 23:50:03 +0900

    add: searTerm model에 getter date 추가

--- client/src/models/searchTerm.ts
+++ client/src/models/searchTerm.ts
@@ -1,9 +1,15 @@
+import formatDate from '../utils/formatDate';
+
 class SearchTerm {
   content: string;
   createdAt: Date;
   constructor(searchTerm: SearchTerm) {
     this.content = searchTerm.content;
     this.createdAt = new Date(searchTerm.createdAt);
+  }
+
+  get date(): string {
+    return formatDate(this.createdAt);
   }
 }
 
