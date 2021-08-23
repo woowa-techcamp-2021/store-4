4ffc3f4250729e4d439dd63b05cb24a9a34100ef
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-23 19:44:41 +0900

    add: Header 로고 클릭 시 메인 이동

--- client/src/components/Header/HeaderMain/HeaderMain.tsx
+++ client/src/components/Header/HeaderMain/HeaderMain.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from '../../../lib/router';
 import CategoryMenu from './CategoryMenu/CategoryMenu';
 import LOGO from './logo.png';
 import SearchBar from './SearchBar/SearchBar';
@@ -37,7 +38,9 @@ const HeaderMain = (): JSX.Element => {
     <Container>
       <CategoryMenu />
       <LogoContainer>
-        <LogoImage src={LOGO} />
+        <Link to="/">
+          <LogoImage src={LOGO} />
+        </Link>
       </LogoContainer>
       <SearchBar />
     </Container>
