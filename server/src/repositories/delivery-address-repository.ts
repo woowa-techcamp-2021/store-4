a4689b1afd8c10059cbd73a46db0d466d8782df7
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-23 23:43:49 +0900

    add: DeliveryAddress 수정 service 메소드 정의

--- server/src/repositories/delivery-address-repository.ts
+++ server/src/repositories/delivery-address-repository.ts
@@ -6,6 +6,13 @@ class DeliveryAddressRepository extends Repository<DeliveryAddress> {
   async findByUser(userId: number): Promise<DeliveryAddress[]> {
     return createQueryBuilder(DeliveryAddress).where('userId = :userId', { userId }).getMany();
   }
+
+  async findOneWithUser(deliveryAddressId: number): Promise<DeliveryAddress | undefined> {
+    return createQueryBuilder(DeliveryAddress)
+      .leftJoinAndSelect('DeliveryAddress.user', 'user')
+      .where('id = :deliveryAddressId', { deliveryAddressId })
+      .getOne();
+  }
 }
 
 export default DeliveryAddressRepository;
--- server/src/services/delivery-address-service.ts
+++ server/src/services/delivery-address-service.ts
@@ -1,10 +1,19 @@
 import { getCustomRepository } from 'typeorm';
+import DeliveryAddressNotfoundException from '../exceptions/delivery-address-notfound-exception';
+import NotMyDeliveryAddressException from '../exceptions/not-my-delivery-address-exception';
 import UserNotfoundException from '../exceptions/user-notfound-exception';
 import DeliveryAddress from '../models/delivery-address';
 import DeliveryAddressRepository from '../repositories/delivery-address-repository';
 import UserRepository from '../repositories/user-repository';
 import { isNone } from '../util/type-guard';
 import DeliveryAddressCreate from '../validations/delivery-address-create';
+import DeliveryAddressModify from '../validations/delivery-address-modify';
+
+const ERROR_MESSAGES = {
+  USER_NOTFOUND: '회원이 존재하지 않습니다.',
+  DELIVERY_ADDRESS_NOTFOUND: '배송지가 존재하지 않습니다',
+  NOT_MY_DELIVERY_ADDRESS: '본인 배송지가 아닙니다',
+};
 
 class DeliveryAddressService {
   async findAll(userId: number): Promise<DeliveryAddress[]> {
@@ -21,11 +30,40 @@ class DeliveryAddressService {
   ): Promise<DeliveryAddress> {
     const user = await getCustomRepository(UserRepository).findOne(userId);
     if (isNone(user)) {
-      throw new UserNotfoundException('유저가 없습니다.');
+      throw new UserNotfoundException(ERROR_MESSAGES.USER_NOTFOUND);
     }
 
     return getCustomRepository(DeliveryAddressRepository).save({
       ...deliveryAddressCreate,
+      user,
+    });
+  }
+
+  async modify(
+    userId: number,
+    deliveryAddressId: number,
+    deliveryAddressModify: DeliveryAddressModify
+  ): Promise<DeliveryAddress> {
+    const user = await getCustomRepository(UserRepository).findOne(userId);
+    if (isNone(user)) {
+      throw new UserNotfoundException(ERROR_MESSAGES.USER_NOTFOUND);
+    }
+
+    const deliveryAddress = await getCustomRepository(DeliveryAddressRepository).findOneWithUser(
+      deliveryAddressId
+    );
+
+    if (isNone(deliveryAddress)) {
+      throw new DeliveryAddressNotfoundException(ERROR_MESSAGES.DELIVERY_ADDRESS_NOTFOUND);
+    }
+
+    if (deliveryAddress.user.id !== user.id) {
+      throw new NotMyDeliveryAddressException(ERROR_MESSAGES.NOT_MY_DELIVERY_ADDRESS);
+    }
+
+    return getCustomRepository(DeliveryAddressRepository).save({
+      id: deliveryAddressId,
+      ...deliveryAddressModify,
       user,
     });
   }
