2e7d7668b0b1830ccc3dde6d5bc367d52362f867
sanginchun <sanginchun91@gmail.com>
2021-08-24 01:02:42 +0900

    fix: 유저에 따른 NavBar 변경 함수화

--- client/src/containers/NavBarContianer.tsx
+++ client/src/containers/NavBarContianer.tsx
@@ -2,14 +2,17 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import NavBar from '../components/Header/NavBar/NavBar';
 import userStore from '../stores/userStore';
+import User from '../models/user';
+import { isNotNone } from '../utils/typeGuard';
+
+const getUserNavItem = (user: User | null) =>
+  isNotNone(user)
+    ? { text: '로그아웃', path: '/logout', badge: null }
+    : { text: '로그인', path: '/login', badge: null };
 
 const NavBarContainer = (): JSX.Element => {
   const navItems = [
-    {
-      text: userStore.user ? '로그아웃' : '로그인',
-      path: userStore.user ? '/logout' : '/login',
-      badge: null,
-    },
+    getUserNavItem(userStore.user),
     { text: '마이페이지', path: '/account', badge: null },
     { text: '장바구니', path: '/cart', badge: 0 },
   ];
