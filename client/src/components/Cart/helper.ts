4c2271f822902b3fbae8e29067608effd445435d
momongss <rkrp15@gmail.com>
2021-08-24 11:29:17 +0900

    add: helper 함수 추가. 전체 옵션받아오기 외

--- client/src/components/Cart/helper.ts
+++ client/src/components/Cart/helper.ts
@@ -1,6 +1,42 @@
 import { SelectWithSelected } from '../../types/product';
 import { isNone } from '../../utils/typeGuard';
 
+export const getOptionList = (selectWithSelecteds: SelectWithSelected[]) => {
+  const optionPriceList = selectWithSelecteds
+    ? getSelectedOptionPriceList(selectWithSelecteds)
+    : [];
+  const optionTypeList = selectWithSelecteds ? getSelectedOptionTypeList(selectWithSelecteds) : [];
+  const optionNameList = selectWithSelecteds ? getSelectedOptionNameList(selectWithSelecteds) : [];
+
+  const optionList = [];
+
+  for (let i = 0; i < selectWithSelecteds.length; i++) {
+    optionList.push({
+      type: optionTypeList[i],
+      name: optionNameList[i],
+      price: optionPriceList[i],
+    });
+  }
+
+  return optionList;
+};
+
+export const getSelectedOptionTypeList = (selectWithSelecteds: SelectWithSelected[]): string[] => {
+  if (isNone(selectWithSelecteds)) {
+    return [];
+  }
+
+  return selectWithSelecteds.map((selected: SelectWithSelected) => selected.name);
+};
+
+export const getSelectedOptionNameList = (selectWithSelecteds: SelectWithSelected[]): string[] => {
+  if (isNone(selectWithSelecteds)) {
+    return [];
+  }
+
+  return selectWithSelecteds.map((selected: SelectWithSelected) => getSelectedOptionName(selected));
+};
+
 export const getSelectedOptionName = (selectWithSelected: SelectWithSelected): string => {
   if (isNone(selectWithSelected)) {
     return '';
@@ -11,6 +47,16 @@ export const getSelectedOptionName = (selectWithSelected: SelectWithSelected): s
   }
 
   return selectWithSelected.selectedOption.name;
+};
+
+export const getSelectedOptionPriceList = (selectWithSelecteds: SelectWithSelected[]): number[] => {
+  if (isNone(selectWithSelecteds) || !Array.isArray(selectWithSelecteds)) {
+    return [];
+  }
+
+  return selectWithSelecteds.map((selected: SelectWithSelected) =>
+    getSelectedOptionPrice(selected)
+  );
 };
 
 export const getSelectedOptionPrice = (selectWithSelected: SelectWithSelected): number => {
