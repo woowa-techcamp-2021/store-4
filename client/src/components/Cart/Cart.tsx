c6f944fb86020d174de5fd8d475c30c2fd0f4494
momongss <rkrp15@gmail.com>
2021-08-24 17:29:01 +0900

    fix: react 모듈 따로 불러오던 것 하나로 합침

--- client/src/components/Cart/Cart.tsx
+++ client/src/components/Cart/Cart.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import CartHeader from './CartHeader/CartHeader';
 import CartTable from './CartTable/CartTable';
