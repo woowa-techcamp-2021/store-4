8afea16f163275006dcdd6475cc3cdfd64b7e6ef
sanginchun <sanginchun91@gmail.com>
2021-08-24 16:29:48 +0900

    add: s3 업로드 전 이미지 처리

--- server/src/services/s3-service.ts
+++ server/src/services/s3-service.ts
@@ -2,7 +2,19 @@ import AWS from 'aws-sdk';
 import { PutObjectRequest } from 'aws-sdk/clients/s3';
 import dotenv from '../config/dotenv';
 import formidable from 'formidable';
-import fs from 'fs';
+import sharp from 'sharp';
+
+const DEFAULT_FILE_TYPE = 'image/webp';
+const DEFAULT_FILE_NAME = 'review';
+const ACCESS_CONTROL = 'public-read';
+
+const S3_PARAMS: PutObjectRequest = {
+  Bucket: dotenv.AWS_S3_BUCKET,
+  ContentType: DEFAULT_FILE_TYPE,
+  ACL: ACCESS_CONTROL,
+  Key: '',
+  Body: undefined,
+};
 
 AWS.config.update({
   accessKeyId: dotenv.AWS_ACCESS_KEY,
@@ -11,22 +23,15 @@ AWS.config.update({
 });
 const S3 = new AWS.S3();
 
-const DEFAULT_FILE_TYPE = 'image/png';
-const ACCESS_CONTROL = 'public-read';
-
 class S3Service {
-  private generateS3Params(imageFile: formidable.File): PutObjectRequest {
-    return {
-      Bucket: dotenv.AWS_S3_BUCKET,
-      Key: Date.now().toString(),
-      ContentType: imageFile.type || DEFAULT_FILE_TYPE,
-      ACL: ACCESS_CONTROL,
-      Body: fs.createReadStream(imageFile.path),
-    };
-  }
-
   async uploadImage(imageFile: formidable.File): Promise<string> {
-    const S3Params = this.generateS3Params(imageFile);
+    const S3Params = { ...S3_PARAMS };
+    S3Params.Key = `${Date.now().toString()}_${
+      imageFile.name?.split('.')[0] || DEFAULT_FILE_NAME
+    }.webp`;
+
+    const sharpImage = await sharp(imageFile.path).resize(500).webp().toBuffer();
+    S3Params.Body = sharpImage;
 
     const data = await S3.upload(S3Params).promise();
     return data.Location;
