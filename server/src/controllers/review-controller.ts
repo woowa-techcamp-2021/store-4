f5ffb04612ce29ddd384efdf4a2d096f6972f6c7
sanginchun <sanginchun91@gmail.com>
2021-08-24 15:52:00 +0900

    add: ReviewController, router 수정

--- server/src/controllers/review-controller.ts
+++ server/src/controllers/review-controller.ts
@@ -1,24 +1,25 @@
 import { Request, Response } from 'express';
 import reviewService from '../services/review-service';
 import { isNone } from '../util/type-guard';
+import ReviewPost from '../validations/review-post';
+import numberParamValidator from '../validations/number-params';
 
 class ReviewController {
   async post(req: Request, res: Response) {
-    const userId = req.decoded?.id;
-    if (isNone(userId)) {
-      res.end();
-      return;
-    }
+    const { userId, productId, point, content } = req.body;
+    const imageLocations = req.images || [];
 
-    const { productId, point, content } = req.body;
-    // validator 추가 필요
+    const reviewPost = new ReviewPost({
+      userId: numberParamValidator(userId),
+      productId: numberParamValidator(productId),
+      point: numberParamValidator(point),
+      content: content || '',
+      imageLocations,
+    } as ReviewPost);
 
-    const review = await reviewService.postReview({
-      userId,
-      productId: +productId,
-      point: +point,
-      content,
-    });
+    await reviewPost.validate();
+
+    const review = await reviewService.postReview(reviewPost);
 
     res.status(201).json({ reviewId: review.id });
   }
