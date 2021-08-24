32a52ee373ee431a6c77c1c4fba7748cc5616adf
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-24 20:19:22 +0900

    add: 회원으로 주문 조회 Service 메소드 생성

--- server/src/repositories/order-repository.ts
+++ server/src/repositories/order-repository.ts
@@ -1,7 +1,16 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { createQueryBuilder, EntityRepository, Repository } from 'typeorm';
 import Order from '../models/order';
 
 @EntityRepository(Order)
-class OrderRepository extends Repository<Order> {}
+class OrderRepository extends Repository<Order> {
+  findWithOrderDetails(userId: number): Promise<Order[]> {
+    return createQueryBuilder(Order)
+      .leftJoinAndSelect('Order.OrderDetails', 'orderDetails')
+      .leftJoinAndSelect('OrderDetails.product', 'orderDetails.product')
+      .where('user_id = :userId', { userId })
+      .orderBy('created_at', 'DESC')
+      .getMany();
+  }
+}
 
 export default OrderRepository;
---
+++ server/src/services/order-service.ts
@@ -0,0 +1,13 @@
+import { getCustomRepository } from 'typeorm';
+import Order from '../models/order';
+import OrderRepository from '../repositories/order-repository';
+
+class OrderService {
+  async findByUser(userId: number): Promise<Order[]> {
+    const orders = getCustomRepository(OrderRepository).findWithOrderDetails(userId);
+
+    return orders;
+  }
+}
+
+export default new OrderService();
