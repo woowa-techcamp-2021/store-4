c3df99722a24a5cdf539f646a2ef3cff38837da9
momongss <rkrp15@gmail.com>
2021-08-24 15:38:14 +0900

    fix: line warning - 사용하지 않는 모듈 삭제

--- client/src/components/Cart/CartTable/TableHeader/TableHeader.tsx
+++ client/src/components/Cart/CartTable/TableHeader/TableHeader.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { observer } from 'mobx-react';
 import cartStore from '../../../../stores/cartStore';
