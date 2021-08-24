212569e7802f77a803117c0315749d953738a78b
sanginchun <sanginchun91@gmail.com>
2021-08-24 13:06:03 +0900

    add: Review api, store 작성 및 post 함수

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,6 +1,7 @@
 import CategoryAPI from './category';
 import ProductAPI from './product';
 import UserAPI from './user';
+import ReviewAPI from './review';
 
 const baseURL = process.env.SERVER_URL as string;
 
@@ -8,6 +9,7 @@ const apis = {
   categoryAPI: new CategoryAPI(baseURL),
   productAPI: new ProductAPI(baseURL),
   userAPI: new UserAPI(baseURL),
+  reviewAPI: new ReviewAPI(baseURL),
 };
 
 export default apis;
