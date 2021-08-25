c0588c42d538faa84f0901a16758ae548d282820
sanginchun <sanginchun91@gmail.com>
2021-08-25 14:10:06 +0900

    add: 응답에 reviewImages, product 추가

--- server/src/repositories/review-repository.ts
+++ server/src/repositories/review-repository.ts
@@ -6,6 +6,8 @@ class ReviewRepository extends Repository<Review> {
   findByUser(userId: number): Promise<Review[]> {
     return createQueryBuilder(Review)
       .leftJoin('Review.user', 'user')
+      .leftJoinAndSelect('Review.reviewImages', 'reviewImages')
+      .leftJoinAndSelect('Review.product', 'product')
       .where('user.id = :userId', { userId })
       .orderBy('Review.createdAt', 'DESC')
       .getMany();
