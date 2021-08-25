f0e0850570a729c5f4cee59ea987174efb601dc0
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-26 02:17:07 +0900

    add: WishStore로 toggle 개발

---
+++ client/src/stores/wishStore.ts
@@ -0,0 +1,18 @@
+import apis from '../api';
+
+class WishStore {
+  async toggle(productId: number, wished: boolean) {
+    const token = localStorage.getItem('token');
+    if (token === null) {
+      return;
+    }
+
+    if (wished) {
+      await apis.productAPI.wish(token, productId);
+    } else {
+      await apis.productAPI.cancelWish(token, productId);
+    }
+  }
+}
+
+export default new WishStore();
