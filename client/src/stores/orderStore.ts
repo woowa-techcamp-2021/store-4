4f5c42afd27c7fb3b7fa3a3c029e19ebc2727d5f
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-25 12:21:21 +0900

    add: replaceList 추가

--- client/src/stores/orderStore.ts
+++ client/src/stores/orderStore.ts
@@ -1,12 +1,31 @@
-import { makeAutoObservable, observable } from 'mobx';
-import OrderDetail from '../models/orderDetail';
+import { action, makeAutoObservable, observable } from 'mobx';
+import CartInProduct from '../models/cart-in-product';
+import OrderDetailProduct from '../models/orderDetailProduct';
+import NOIMAGE from '../assets/images/no-image.png';
 
 class OrderStore {
   @observable
-  orderDetailLists: OrderDetail[] = [];
+  orderDetailProductLists: OrderDetailProduct[] = [];
 
   constructor() {
     makeAutoObservable(this);
+  }
+
+  @action
+  replaceList(cartInProducts: CartInProduct[]) {
+    this.orderDetailProductLists = cartInProducts.map((cartInProduct) => {
+      const { product, options, count } = cartInProduct;
+
+      return new OrderDetailProduct({
+        id: product.id,
+        name: product.name,
+        count,
+        thumbnail: product.thumbnail || NOIMAGE,
+        price: cartInProduct.totalPrice,
+        selectWithSelecteds: options,
+      });
+    });
+    console.log(this.orderDetailProductLists);
   }
 }
 
