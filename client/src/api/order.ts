fc71477553c6d82716881210a31e39ceacd15a0c
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-26 03:01:49 +0900

    add: CreateOrder API 구현

---
+++ client/src/api/order.ts
@@ -0,0 +1,24 @@
+import request from '../lib/request';
+import { CreateOrderRequest } from '../types/order';
+
+class OrderAPI {
+  private baseURL: string;
+
+  constructor(baseURL: string) {
+    this.baseURL = baseURL;
+  }
+
+  createOrder(token: string, data: CreateOrderRequest): Promise<void> {
+    return request<void>({
+      url: `${this.baseURL}/order`,
+      method: 'POST',
+      body: data,
+      headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+        Authorization: token,
+      },
+    });
+  }
+}
+
+export default OrderAPI;
