029e9018606ebc06070e60ee33720bcf365321b0
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-26 00:44:40 +0900

    add: 장바구니에서 주문하기 버튼들 동작 추가

--- client/src/containers/CartContainer.tsx
+++ client/src/containers/CartContainer.tsx
@@ -1,12 +1,34 @@
+import { observer } from 'mobx-react';
 import React from 'react';
 import Cart from '../components/Cart/Cart';
+import { useHistory } from '../lib/router';
+import cartStore from '../stores/cartStore';
+import orderStore from '../stores/orderStore';
 
 const CartContainer = (): JSX.Element => {
+  const history = useHistory();
+
   const handleClickAllProductOrderButton = () => {
+    if (cartStore.isNothingSelectedCartItems) {
+      alert('선택된 아이템이 없습니다.');
+      return;
+    }
+
+    orderStore.replaceListTocartItemList = cartStore.selectedCartItemList;
+    history.push('/order');
+
     return;
   };
 
   const handleClickSelectedProductOrderButton = () => {
+    if (cartStore.isNothingSelectedCartItems) {
+      alert('선택된 아이템이 없습니다.');
+      return;
+    }
+
+    orderStore.replaceListTocartItemList = cartStore.selectedCartItemList;
+    history.push('/order');
+
     return;
   };
 
@@ -18,4 +40,4 @@ const CartContainer = (): JSX.Element => {
   );
 };
 
-export default CartContainer;
+export default observer(CartContainer);
