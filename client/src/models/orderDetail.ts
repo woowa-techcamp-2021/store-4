3b5bef9e826281da72f6b46bd0c277e7ed335001
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-25 11:21:09 +0900

    add: OrderDetail property 정의

--- client/src/models/orderDetail.ts
+++ client/src/models/orderDetail.ts
@@ -1,3 +1,19 @@
-class OrderDetail {}
+import { SelectWithSelected } from '../types/product';
+
+class OrderDetail {
+  id: number;
+  name: string;
+  price: number;
+  thumbnail: string;
+  selectWithSelecteds?: SelectWithSelected[];
+
+  constructor(orderDetail: OrderDetail) {
+    this.id = orderDetail.id;
+    this.name = orderDetail.name;
+    this.price = orderDetail.price;
+    this.thumbnail = orderDetail.thumbnail;
+    this.selectWithSelecteds = orderDetail.selectWithSelecteds;
+  }
+}
 
 export default OrderDetail;
