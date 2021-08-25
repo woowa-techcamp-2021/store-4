757a99744c5d2d6b4d4a5069688180de86a3e08a
sanginchun <sanginchun91@gmail.com>
2021-08-25 14:42:41 +0900

    add: Review api, store 함수

--- client/src/api/review.ts
+++ client/src/api/review.ts
@@ -1,10 +1,17 @@
 import request from '../lib/request';
+import { ReviewsByUserResponse } from '../types/review';
 
 class ReviewAPI {
   private baseURL: string;
 
   constructor(baseURL: string) {
     this.baseURL = baseURL;
+  }
+
+  getReviewsByUser(userId: number): Promise<ReviewsByUserResponse> {
+    return request<ReviewsByUserResponse>({
+      url: `${this.baseURL}/api/review/user/${userId}`,
+    });
   }
 
   postReview(formData: FormData, token: string): Promise<void> {
--- client/src/stores/reviewStore.ts
+++ client/src/stores/reviewStore.ts
@@ -1,6 +1,11 @@
 import apis from '../api';
+import { ReviewsByUserResponse } from '../types/review';
 
 class ReviewStore {
+  getReviewsByUser(userId: number): Promise<ReviewsByUserResponse> {
+    return apis.reviewAPI.getReviewsByUser(userId);
+  }
+
   postReview(formData: FormData, token: string): Promise<void> {
     return apis.reviewAPI.postReview(formData, token);
   }
