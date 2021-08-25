46b868dc21eda138fe600f98ced0a9279619253b
sanginchun <sanginchun91@gmail.com>
2021-08-25 13:43:44 +0900

    add: Review 레파지토리 findByUser

--- server/src/repositories/review-repository.ts
+++ server/src/repositories/review-repository.ts
@@ -1,7 +1,15 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, Repository, createQueryBuilder } from 'typeorm';
 import Review from '../models/review';
 
 @EntityRepository(Review)
-class ReviewRepository extends Repository<Review> {}
+class ReviewRepository extends Repository<Review> {
+  findByUser(userId: number): Promise<Review[]> {
+    return createQueryBuilder(Review)
+      .leftJoin('Review.user', 'user')
+      .where('user.id = :userId', { userId })
+      .orderBy('Review.createdAt', 'DESC')
+      .getMany();
+  }
+}
 
 export default ReviewRepository;
