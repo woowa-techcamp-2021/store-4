714312ef69cfd4e15eb3b88cee765f97fb057643
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-26 14:05:52 +0900

    add: 인증 관련 컴포넌트 개발

---
+++ client/src/components/Authentication/Authentication.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { createContext } from 'react';
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { useHistory } from '../../lib/router';
+import toast from '../../lib/toast';
+import userStore from '../../stores/userStore';
+import { isNone } from '../../utils/typeGuard';
+
+type Props = {
+  children: JSX.Element | JSX.Element[];
+};
+
+type AuthenticationContextType = {
+  onErrorOccurred: () => void;
+};
+
+export const AuthenticationContext = createContext({} as AuthenticationContextType);
+
+const AuthenticationProvider = ({ children }: Props): JSX.Element => {
+  const [isErrorOccurred, setIsErrorOccurred] = useState(false);
+  const routerHistory = useHistory();
+
+  const handleOccurred = () => {
+    console.log('pass');
+    setIsErrorOccurred(true);
+  };
+
+  useEffect(() => {
+    if (isNone(localStorage.getItem('token'))) {
+      toast.error('로그인이 필요합니다');
+      history.back();
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isErrorOccurred) {
+      toast.error('로그인이 필요합니다');
+      userStore.logoutUser();
+      routerHistory.push('/login');
+    }
+  }, [isErrorOccurred, routerHistory]);
+
+  const value = {
+    onErrorOccurred: handleOccurred,
+  };
+
+  return <AuthenticationContext.Provider value={value}>{children}</AuthenticationContext.Provider>;
+};
+
+export default AuthenticationProvider;
