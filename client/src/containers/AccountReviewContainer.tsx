61f10b6e6eddc599fc10e3a98a29a124667774d2
sanginchun <sanginchun91@gmail.com>
2021-08-26 15:47:29 +0900

    add: 마이페이지 상품후기 삭제 alert -> toast

--- client/src/containers/AccountReviewContainer.tsx
+++ client/src/containers/AccountReviewContainer.tsx
@@ -3,10 +3,13 @@ import { ReviewWithProduct } from '../models/review';
 import AccountReview from '../components/Account/AccountReview/AccountReview';
 import reviewStore from '../stores/reviewStore';
 import userStore from '../stores/userStore';
+import { useHistory } from '../lib/router';
+import toastHelper from '../lib/toast';
 
 const AccountReviewContainer = (): JSX.Element => {
   const userId = userStore.user?.id;
   const [reviews, setReviews] = useState<ReviewWithProduct[]>([]);
+  const history = useHistory();
 
   useEffect(() => {
     if (userId === undefined) {
@@ -19,21 +22,35 @@ const AccountReviewContainer = (): JSX.Element => {
   }, [userId]);
 
   const handleDeleteReviewClick = (deleteReviews: ReviewWithProduct[]) => {
-    if (deleteReviews.length === 0) {
-      alert('삭제할 후기를 선택하세요');
+    const reviewsLength = deleteReviews.length;
+    if (reviewsLength === 0) {
+      toastHelper.info('삭제할 후기를 선택하세요');
       return;
     }
 
-    if (window.confirm(`${deleteReviews.length}개의 상품 후기를 삭제하시겠습니까?`)) {
+    if (window.confirm(`${reviewsLength}개의 상품 후기를 삭제하시겠습니까?`)) {
       reviewStore
         .deleteReview(deleteReviews.map((review) => review.id))
         .then(() => {
-          const nextReviews = reviews.filter((review) => !deleteReviews.includes(review));
+          toastHelper.success(`${reviewsLength}개의 후기가 삭제되었습니다`);
 
+          const nextReviews = reviews.filter((review) => !deleteReviews.includes(review));
           setReviews(nextReviews);
         })
-        .catch(() => {
-          alert('죄송합니다. 삭제에 실패했습니다');
+        .catch((error) => {
+          switch (error.status) {
+            case 401:
+              toastHelper.error('해당 후기에 대한 권한이 없습니다');
+              history.push('/account');
+              return;
+            case 404:
+              toastHelper.error('이미 삭제된 후기입니다');
+              history.push('/account/reviews');
+              return;
+            default:
+              toastHelper.error('오류가 발생했습니다');
+              return;
+          }
         });
     }
   };
