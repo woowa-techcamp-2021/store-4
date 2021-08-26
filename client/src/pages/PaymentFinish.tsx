cf3bd52084e345b654eab9882a07ba4e478d5649
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-26 15:56:53 +0900

    add: Order에 paymentfinish를 옮기면서 page 삭제

--- client/src/pages/PaymentFinish.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import PaymentFinish from '../components/PaymentFinish/PaymentFinish';
-
-const PaymentFinishPage = (): JSX.Element => {
-  return <PaymentFinish />;
-};
-
-export default PaymentFinishPage;
