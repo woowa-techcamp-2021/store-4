7b6cf43e096bb1cf8d6a7d4896ef95d25547ceda
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-26 21:58:52 +0900

    add: token 검사 부분을 useEffect을 삭제

--- client/src/components/Authentication/Authentication.tsx
+++ client/src/components/Authentication/Authentication.tsx
@@ -26,12 +26,9 @@ const AuthenticationProvider = ({ children }: Props): JSX.Element => {
     setIsErrorOccurred(true);
   };
 
-  useEffect(() => {
-    if (isNone(localStorage.getItem('token'))) {
-      toast.error('로그인이 필요합니다');
-      history.back();
-    }
-  }, []);
+  const value = {
+    onErrorOccurred: handleOccurred,
+  };
 
   useEffect(() => {
     if (isErrorOccurred) {
@@ -41,9 +38,12 @@ const AuthenticationProvider = ({ children }: Props): JSX.Element => {
     }
   }, [isErrorOccurred, routerHistory]);
 
-  const value = {
-    onErrorOccurred: handleOccurred,
-  };
+  if (isNone(localStorage.getItem('token'))) {
+    toast.error('로그인이 필요합니다');
+    routerHistory.push('/login');
+
+    return <></>;
+  }
 
   return <AuthenticationContext.Provider value={value}>{children}</AuthenticationContext.Provider>;
 };
