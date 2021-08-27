14c5024b46f8a33a7e08a48bb490bfd7a3c89641
sanginchun <sanginchun91@gmail.com>
2021-08-27 13:00:51 +0900

    fix: 테스트 코드 수정

--- client/src/components/Login/Login.test.tsx
+++ client/src/components/Login/Login.test.tsx
@@ -5,21 +5,20 @@ import provideTheme2Test from '../../lib/provideTheme2Test';
 import { OAuthType } from '../../containers/LoginContainer';
 
 describe('Login 컴포넌트', () => {
-  const FB_LOGIN_CONTENT = '페이스북';
+  // const FB_LOGIN_CONTENT = '페이스북';
   const GOOGLE_LOGIN_CONTENT = '구글';
+  const DEMO_LOGIN_CONTENT = '데모';
   const BUTTONS = {
-    [OAuthType.Facebook]: {
-      backgroundColor: '#4267b2',
-      fontColor: '#ffffff',
-      icon: '',
-      content: FB_LOGIN_CONTENT,
-      redirectURL: `${process.env.SERVER_URL}/auth/facebook-login`,
-    },
+    // [OAuthType.Facebook]: {
+    //   content: FB_LOGIN_CONTENT,
+    //   redirectURL: `${process.env.SERVER_URL}/auth/facebook-login`,
+    // },
     [OAuthType.Google]: {
-      backgroundColor: '#EA4335',
-      fontColor: '#ffffff',
-      icon: '',
       content: GOOGLE_LOGIN_CONTENT,
+      redirectURL: `${process.env.SERVER_URL}/auth/google-login`,
+    },
+    [OAuthType.Demo]: {
+      content: DEMO_LOGIN_CONTENT,
       redirectURL: `${process.env.SERVER_URL}/auth/google-login`,
     },
   };
