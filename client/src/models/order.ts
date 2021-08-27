d8598758190ad929e032105994b1af37dfd435c7
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-28 00:30:23 +0900

    add: Order Model에 createdAt 추가

--- client/src/models/order.ts
+++ client/src/models/order.ts
@@ -6,6 +6,7 @@ class Order {
   address: string;
   recipientName: string;
   orderDetails: OrderDetail[];
+  createdAt: Date;
 
   constructor(order: Order) {
     this.id = order.id;
@@ -13,6 +14,7 @@ class Order {
     this.address = order.address;
     this.recipientName = order.recipientName;
     this.orderDetails = order.orderDetails.map((orderDetail) => new OrderDetail(orderDetail));
+    this.createdAt = new Date(order.createdAt);
   }
 }
 
