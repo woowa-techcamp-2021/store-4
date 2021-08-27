3156a2686d9066d6b5d96edd80daf27c30c6e6b7
sanginchun <sanginchun91@gmail.com>
2021-08-28 04:19:35 +0900

    fix: account 컨테이너 로직 분리 (조건부 링크, 토스트)

--- client/src/containers/NavBarContianer.tsx
+++ client/src/containers/NavBarContianer.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import userStore from '../stores/userStore';
+import cartStore from '../stores/cartStore';
 import { observer } from 'mobx-react';
 import NavBar from '../components/Header/NavBar/NavBar';
-import userStore from '../stores/userStore';
+import Badge from '../components/Header/Badge/Badge';
 import User from '../models/user';
 import { isNotNone } from '../utils/typeGuard';
-import cartStore from '../stores/cartStore';
+import { Link, ConditionLink } from '../lib/router';
+import toast from '../lib/toast';
 
-const getUserNavItem = (user: User | null) =>
-  isNotNone(user)
-    ? { text: '로그아웃', path: '/logout', badge: null }
-    : { text: '로그인', path: '/login', badge: null };
+const getUserNavItem = (user: User | null) => {
+  return isNotNone(user) ? <Link to="/logout">로그아웃</Link> : <Link to="/login">로그인</Link>;
+};
 
 const NavBarContainer = (): JSX.Element => {
-  const cartItems = cartStore.cartItemList;
+  const cartItemCount = cartStore.cartItemList.length;
+  const { user } = userStore;
 
-  const navItems = [
-    getUserNavItem(userStore.user),
-    { text: '마이페이지', path: '/account', badge: null },
-    { text: '장바구니', path: '/cart', badge: cartItems.length },
+  const handleAccountLinkClick = () => {
+    if (isNotNone(user)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  const handleFail = () => toast.info('로그인이 필요합니다');
+
+  const navLinks = [
+    getUserNavItem(user),
+    <ConditionLink key={1} to="/account" onClick={handleAccountLinkClick} onFail={handleFail}>
+      마이페이지
+    </ConditionLink>,
+    <Fragment key={2}>
+      <Link to="/cart">장바구니</Link>
+      <Badge>{cartItemCount}</Badge>
+    </Fragment>,
   ];
 
-  return <NavBar navItems={navItems} />;
+  return <NavBar links={navLinks} />;
 };
 
 export default observer(NavBarContainer);
