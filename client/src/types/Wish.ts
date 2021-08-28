16f6bfb52777364400ee6fb3e4e0382895985c13
momongss <rkrp15@gmail.com>
2021-08-28 16:40:28 +0900

    add: WishResponse 타입 선언

--- client/src/types/Wish.ts
+++ client/src/types/Wish.ts
@@ -1,5 +1,23 @@
 export type Wish = {
   id: number;
+  productId: number;
   title: string;
   imgSrc: string;
+};
+
+export type WishResponse = {
+  id: number;
+  product: Product;
+};
+
+type Product = {
+  id: number;
+  name: string;
+  price: string;
+  productImages: ProductImage[];
+};
+
+type ProductImage = {
+  id: number;
+  url: string;
 };
