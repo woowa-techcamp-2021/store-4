35b0cd251362df426408310e3063b5d977f62cf3
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-28 14:48:46 +0900

    add: Summary, Detail로 나눔(detail은 상품 목록)

--- client/src/components/Account/AccountOrder/AccountOrderTable/AccountOrderTableItem.tsx
+++ client/src/components/Account/AccountOrder/AccountOrderTable/AccountOrderTableItem.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState, useCallback } from 'react';
 import styled from 'styled-components';
 import Order from '../../../../models/order';
 import { getDate } from '../../../../utils/formatDate';
 import { toKoreanMoneyFormat } from '../../../../utils/moneyFormater';
+import AccountOrderTableItemDetail from './AccountOrderTableItemDetail';
+import { RiArrowDownSLine } from 'react-icons/ri';
 
-export const Container = styled.div`
+export const Container = styled.div``;
+
+const AccountOrderTableItemSummary = styled.div`
+  flex: 1;
   display: flex;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 const Column = styled.div`
@@ -39,6 +47,19 @@ export const ItemCount = styled(Column)`
   flex: 1;
 `;
 
+const ArrowDownWrapper = styled(Column)`
+  flex: 1;
+`;
+
+type SeemoreProps = {
+  isCloseIcon: boolean;
+};
+
+const Seemore = styled.div<SeemoreProps>`
+  transform: rotate(${(props) => (props.isCloseIcon ? '-180' : '0')}deg);
+  transition: transform 0.3s ease-in-out;
+`;
+
 type Props = {
   order: Order;
 };
@@ -46,13 +67,28 @@ type Props = {
 const AccountOrderTableItem = (props: Props): JSX.Element => {
   const { order } = props;
   const { recipientName, address, totalPrice, orderDetails, createdAt } = order;
+  const [orderDetailOpen, setOrderDetailOpen] = useState(false);
+
+  const handleReviewSummaryClick = useCallback(
+    () => setOrderDetailOpen((orderDetailOpen) => !orderDetailOpen),
+    []
+  );
+
   return (
     <Container>
-      <Date>{getDate(createdAt)}</Date>
-      <Recipient>{recipientName}</Recipient>
-      <Address>{address}</Address>
-      <ItemCount>{`${orderDetails.length}개`}</ItemCount>
-      <TotalPrice>{toKoreanMoneyFormat(totalPrice)}</TotalPrice>
+      <AccountOrderTableItemSummary onClick={handleReviewSummaryClick}>
+        <Date>{getDate(createdAt)}</Date>
+        <Recipient>{recipientName}</Recipient>
+        <Address>{address}</Address>
+        <ItemCount>{`${orderDetails.length}개`}</ItemCount>
+        <TotalPrice>{toKoreanMoneyFormat(totalPrice)}</TotalPrice>
+        <ArrowDownWrapper>
+          <Seemore isCloseIcon={orderDetailOpen}>
+            <RiArrowDownSLine />
+          </Seemore>
+        </ArrowDownWrapper>
+      </AccountOrderTableItemSummary>
+      {orderDetailOpen && <AccountOrderTableItemDetail orderDetails={orderDetails} />}
     </Container>
   );
 };
