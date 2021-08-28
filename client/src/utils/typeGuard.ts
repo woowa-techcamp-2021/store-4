d51ca91deb3bb290271f1c66dccdc3a126183551
sanginchun <sanginchun91@gmail.com>
2021-08-28 13:42:47 +0900

    add: 타입가드 추가

--- client/src/utils/typeGuard.ts
+++ client/src/utils/typeGuard.ts
@@ -9,3 +9,23 @@ export const isNotNone = <T>(value: T | undefined | null): value is T => {
 export const isNumber = (value: unknown): value is number => {
   return !Number.isNaN(value);
 };
+
+export const isPositiveInteger = (value: unknown): value is number => {
+  if (!isNumber(value)) {
+    return false;
+  }
+
+  if (!Number.isInteger(value)) {
+    return false;
+  }
+
+  if (value <= 0) {
+    return false;
+  }
+
+  return true;
+};
+
+export const isNotPositiveInteger = <T>(value: T): value is T => {
+  return !isPositiveInteger(value);
+};
