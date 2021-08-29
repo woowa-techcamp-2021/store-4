6540e4168df0b424f20ab2213ea09aa7bee24c70
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-08-29 21:58:30 +0900

    add: Webpack에서 node_modules vendor 설정

--- client/webpack.common.js
+++ client/webpack.common.js
@@ -23,7 +23,7 @@ module.exports = {
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    filename: '[name].[chunkhash].js',
     clean: true,
     publicPath: '/',
   },
