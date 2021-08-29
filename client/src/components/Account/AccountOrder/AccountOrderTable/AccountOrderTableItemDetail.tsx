067756c7978df6ea0618045a5298d7587b48f377
Juyoung Park <juyoung7018.park@gmail.com>
2021-08-29 21:28:57 +0900

    add: AccountOrderTableItem Detail에 lazy img 컴포넌트 적용

--- client/src/components/Account/AccountOrder/AccountOrderTable/AccountOrderTableItemDetail.tsx
+++ client/src/components/Account/AccountOrder/AccountOrderTable/AccountOrderTableItemDetail.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
 import OrderDetail from '../../../../models/orderDetail';
-import NO_IMAGE from '../../../../assets/images/no-image.png';
 import { toKoreanMoneyFormat } from '../../../../utils/moneyFormater';
 import { Link } from '../../../../lib/router';
+import LazyImage from '../../../LazyImage/LazyImage';
 
 const Container = styled.div`
   border-bottom: 1px solid ${(props) => props.theme.color.grey2};
@@ -71,10 +71,11 @@ const AccountOrderTableItemDetail = (props: Props): JSX.Element => {
       <Row key={orderDetail.id}>
         <ProductThumbnailWrapper>
           <Link to={`/product/${orderDetail.product.id}`}>
-            <ProductThumbnail
+            <LazyImage
+              width={48}
+              aspectRatio={[4, 5]}
+              src={orderDetail.product.thumbnail}
               alt="상품 이미지"
-              referrerPolicy="no-referrer"
-              src={orderDetail.product.thumbnail || NO_IMAGE}
             />
           </Link>
         </ProductThumbnailWrapper>
