3893f3df8087f3679d76c7aa9c5ca562d1548f69
sanginchun <sanginchun91@gmail.com>
2021-08-29 22:11:11 +0900

    fix: withAuthentication hoc 수정 - userStore에 status 정의 - user, status에 따라 컴포넌트 리턴, 토스트, history.replace - path 받을 필요 없어짐에 따라 페이지 컴포넌트 수정

--- client/src/hoc/withAuthentication.tsx
+++ client/src/hoc/withAuthentication.tsx
@@ -6,21 +6,22 @@ import { observer } from 'mobx-react';
 import { useHistory } from '../lib/router';
 
 const withAuthentication = <P,>(
-  WrappedComponent: React.ComponentType<P>,
-  path: string
+  WrappedComponent: React.ComponentType<P>
 ): ((props: P) => JSX.Element) => {
   const WithAuth = (props: P) => {
-    const { user } = userStore;
+    const { user, status } = userStore;
     const history = useHistory();
 
     useEffect(() => {
-      if (user === null) {
+      if (status === 'DONE' && user === null) {
         toast.info('로그인이 필요합니다');
+        history.replace('/login');
       }
+    }, [status, user, history]);
 
-      const replacePath = user === null ? 'login' : path;
-      history.replace(`/${replacePath}`);
-    }, [user, history]);
+    if (status !== 'DONE') {
+      return <></>;
+    }
 
     if (user === null) {
       return <LoginPage />;
